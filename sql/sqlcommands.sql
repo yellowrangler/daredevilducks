361c82de7b0b788b5f54dbed5def8e7c2114d3b6
Tarrant Cutler Jr <tarrant.cutler@gmail.com>
2016-12-04 16:39:11 -0500

    short nav gui icon padding change

--- sql/sqlcommands.sql
+++ sql/sqlcommands.sql
@@ -6,41 +6,29 @@ mysqldump --extended-insert=FALSE -u tarryc -p ddd > ddd-dump-08082015.sql
 mysqldump --complete-insert -u tarryc -p ddd > ddd-dump-08082015.sql
 mysqldump --complete-insert --skip-extended-insert -u tarryc -p ddd > ddd-dump-08092015.sql
 
-SELECT count(distinct week) as weeks
-FROM gamestbl where season = 2014;
+SELECT table_schema "Data Base Name", sum( data_length + index_length) / 1024 / 1024
+"Data Base Size in MB" FROM information_schema.TABLES GROUP BY table_schema ;
 
-SELECT max(week) as weeks
-FROM gamestbl where season = 2014;
+SELECT  memberid, membername, COUNT(*) as picks
+FROM memberpickstbl MP
+LEFT JOIN membertbl M on MP.memberid = M.id
+WHERE season = 2016 and week = 8
+GROUP BY membername, memberid
 
-SELECT count(*) as games
-FROM gamestbl where hometeamid = 1 or awayteamid = 1;
+UNION 
 
-SELECT count(*) as wins
-FROM gamestbl where season = 2014 and week <= 17 
-and (
-(hometeamid = 5 and hometeamscore > awayteamscore)
-or (awayteamid = 5 and awayteamscore > hometeamscore)
-);
+SELECT  DISTINCT id as memberid, membername, CAST('0' AS INT) as picks
+FROM membertbl 
+WHERE id NOT IN 
+(
+	SELECT  memberid as id
+	FROM memberpickstbl MP
+	LEFT JOIN membertbl M on MP.memberid = M.id
+	WHERE season = 2016 and week = 8
+)
+AND status = 'active'
 
-SELECT count(*) as losses
-FROM gamestbl where (season = 2014 and week <= 17) 
-and (
-(hometeamid = 5 and awayteamscore > hometeamscore)
-or (awayteamid = 5 and hometeamscore > awayteamscore)
-);
 
-SELECT count(*) as ties
-FROM gamestbl where (season = 2014 and week <= 17) 
-and (
-(hometeamid = 5 and awayteamscore = hometeamscore)
-or (awayteamid = 5 and hometeamscore = awayteamscore)
-);
-
--- SELECT count(*) as games
--- FROM gamestbl where (season = 2014 and week <= 17)
--- and (
--- (hometeamid = 1)
--- or (awayteamid = 1)
--- );
+DELETE FROM memberpickstbl WHERE season = 2016 and week = 13 and memberid = 37
 
 
