1cd183bb47516c9497d59420f3128695d5eaa6a7
Tarrant Cutler Jr <tarrant.cutler@gmail.com>
2016-12-19 20:05:49 -0500

    Added animation snowfall to page. Can start and stop by Christmas tree icon bottom left.

--- app/controllers/parenthomeController.js
+++ app/controllers/parenthomeController.js
@@ -57,8 +57,84 @@ controllers.dddParentController = function ($scope, $http, $window, $route, $loc
         }
     }
 
+    function letItSnow () 
+    {
+        if ($scope.isSnowing == 0)
+        {
+            $("#letitsnowtext").text("Turn off snow!");
+            
+            makeSnowAnimation({ minSize: 5, maxSize: 50, newOn: 1000, flakeColor: '#2ECC71' });
+
+            $scope.isSnowing = 1;
+        }
+        else
+        {
+            $("#letitsnowtext").text("Let it Snow!");
+
+            stopSnowAnimation();
+            $scope.isSnowing = 0;
+        }
+        
+    }
+
+    function stopSnowAnimation()
+    {
+        clearInterval($scope.intervalVariable); 
+    }
+
+    function makeSnowAnimation(options)
+    {
+        var $flake          = $('<div id="flake" />').css({'position': 'absolute', 'top': '-50px'}).html('&#10052;'),
+            documentHeight  = $(document).height(),
+            documentWidth   = $(document).width(),
+            defaults        = {
+                                minSize     : 10,
+                                maxSize     : 20,
+                                newOn       : 500,
+                                flakeColor  : "#FFFFFF"
+                            },
+            options         = $.extend({}, defaults, options);
+            
+        $scope.intervalVariable        = 
+            setInterval( function(){
+                var startPositionLeft   = Math.random() * documentWidth - 100,
+                    startOpacity        = 0.5 + Math.random(),
+                    sizeFlake           = options.minSize + Math.random() * options.maxSize,
+                    endPositionTop      = documentHeight - 40,
+                    endPositionLeft     = startPositionLeft - 100 + Math.random() * 200,
+                    durationFall        = documentHeight * 10 + Math.random() * 5000;
+                $flake
+                    .clone()
+                    .appendTo('body')
+                    .css(
+                        {
+                            left: startPositionLeft,
+                            opacity: startOpacity,
+                            'font-size': sizeFlake,
+                            color: options.flakeColor
+                        }
+                    )
+                    .animate(
+                        {
+                            top: endPositionTop,
+                            left: endPositionLeft,
+                            opacity: 0.2
+                        },
+                        durationFall,
+                        'linear',
+                        function() {
+                            $(this).remove()
+                        }
+                    );
+            }, options.newOn);
+    }
+
     init();
     function init() {
+        $scope.isSnowing = 0;
+        $scope.intervalVariable = "";
+        $("#letitsnowtext").text("Let it Snow!");
+
         $scope.current = {};
         $scope.bigNavbar = true;
 
@@ -193,6 +269,10 @@ controllers.dddParentController = function ($scope, $http, $window, $route, $loc
         $('#parentAlertModalTitle').text(title);
         $('#parentAlertModalBody').text(body);
         $('#parentAlertModal').modal();
+    }
+
+    $scope.letItSnow = function() {
+        letItSnow();
     }
 }
 
