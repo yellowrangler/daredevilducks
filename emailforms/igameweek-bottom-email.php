ac519f1846a36bbddfbf272bce365c53ca72ae5b
tarryc <tarryc@manchester.westell.com>
2017-01-18 15:23:02 -0500

    dump and email 01182017

--- emailforms/igameweek-bottom-email.php
+++ emailforms/igameweek-bottom-email.php
@@ -4,35 +4,9 @@ $emailbottomwriteup = "
 <br />
 
 <p>
-<span  style='font-weight:bold;color:red;'>Note: Regular season is over. Playoffs start this Saturday and Sunday!</span>
+<span  style='font-weight:bold;color:red;'>Note: Regular season is over. Playoffs are Sunday!</span>
 </p>
 
-<p>
-<span  style='font-weight:bold;color:green;'>Mobile changes</span>
-</p>
-
-
-<p>
-The Avatar at the bottom of the page will now scroll horizontally. Scrolling right to left will reveal a Christmas tree and the a New Years donald duck image. Clicking on the Christmas tree will start the snowfall animation. Clicking on the Christmass tree with the international symbol for NO on it will stop the animation! Click on the New Years donald duck  Image and you will get fireworks!
-</p>
-
-<p>
-<span  style='font-weight:bold;color:green;'>Snow Flake Animation</span>
-</p>
-
-
-<p>
-We added a Christmas tree at the bottom of our web page. Clicking on the Christmas tree will start the snowfall animation. Clicking on the Christmass tree with the international symbol for NO on it will stop the animation! 
-</p>
-
-<p>
-<span  style='font-weight:bold;color:green;'>Fireworks Animation</span>
-</p>
-
-
-<p>
-Click on the <span style='font-weight:bold; color:blue;'>Home Page 2017 New Year</span> Image and you will get a web page with fireworks! We removed the popup window.
-</p>
 
 <p>
 <span  style='font-weight:bold;color:green;'>Please set up your profile!</span>
@@ -41,14 +15,5 @@ Click on the <span style='font-weight:bold; color:blue;'>Home Page 2017 New Year
 <p>
 Your player profile can be accessed by clicking on your avatar at the top right of the Daredevil Ducks web page. Add some information about yourself! Go to the viewing permissions check boxes at the bottom of the page and open up what you feel comfortable sharing with people,
 </p>
-
-
-<p>
-<span  style='font-weight:bold;color:green;'>New Video available on How to Login and make your picks</span>
-</p>
-
-<p>
-I have posted a video on youtube on how to login and make your game picks. You can See the video at https://youtu.be/YG9ZleppJwo
-</p>";
-
+";
 ?>
--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -9,25 +9,26 @@ Hi Folks,
 <br />
 
 <p>
-The playoff gods have spoken! This week was not as competative as one would have hoped. Most games were a statement making beatdown. 
+What a great week of playoffs! Now we enter into the final conference challendes from which will emerge our Super Bowl contenders!
 </p>
 
 <p>
-The Car-less Raiders just could not get into gear to beat a very animic Texas team. The only person on the Lions team who showed up was the quaterback. Though there were some crucial questionable calls that went Seattle's way in that game (4th down scoring fasemask anyone?). The Steelers looked VERY scarey as they pumled the Dolphins. Elli Manning saw his heralded (well tanned - Ha) recievers unable to make big plays even when the ball hit them squarely on the hands. Rogers had no such problem - as the Packers quietly dispathched the Giants.
+The Packers did it again with their wylie quaterback. Do not despair Dallas fans; they are a young team and should be back. BUt what a great game! The Steelers and Chiefs broght it down to the last second. All in all teh bext teams in football are left to slug it out.
 </p>
 
 <p>
-So now the real fun begins. Time for the cream of the NFL to get churned! Wildcat Wonder is working on the updated brackets as I write this. Same format next week - 2 games on Saturday and 2 games on Sunday.</p>
+So the real fun continues. Wildcat Wonder has finished and uploaded the updated brackets. The new stats are updated and in. The format for next week is 2 games on Sunday.</p>
 
 <p>
-We had another great turnout this week. Thank you all again for participating. We have a 3 way tie as 18 Wheeler,  Toothfairy and Wildcat Wonder share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by Exploding Brain in second place and Empty Nesters and NFL Amateur in third. 
+We had another great turnout this week. Thank you all again for participating. We have a 2 way tie as 18 Wheeler, and Toothfairy share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by Wildcat Wonder in second place and Exploding Brain in third. 
 </p>
 
 <p>
-Johor Tigress retaines command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. She is followed by 18 Wheeler in second and Exploding Brain in third.
+Johor Tigress retaines command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. She is followed by 18 Wheeler in second and Toothfairy in third.
 </p>
+
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have more folks then will fit on this line! Check them out fllowing! Way to go folks! The stats are below. Read em and weep (I know I do)!
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have 18 Wheeler, Super Brawler, The Fighting Ducks and Toothfairy. Check them out fllowing! Way to go folks! The stats are below. Read em and weep (I know I do)!
 </p>  
 ";
 ?>
