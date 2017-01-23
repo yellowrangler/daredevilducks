7cf70194b69ac1dd36344d5fd5539fd9acb99785
tarryc <tarryc@manchester.westell.com>
2017-01-23 16:08:07 -0500

    dump 01232017

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -9,18 +9,22 @@ Hi Folks,
 <br />
 
 <p>
-What a great week of playoffs! Now we enter into the final conference challendes from which will emerge our Super Bowl contenders!
+<span style='font-weight:bold; color:red;'>Someone fat fingered a score and that correction has changed our standings. That someone has been severely reprimanded and he promises to be more careful!</span>
 </p>
 
 <p>
-The Packers did it again with their wylie quaterback. Do not despair Dallas fans; they are a young team and should be back. BUt what a great game! The Steelers and Chiefs broght it down to the last second. All in all teh bext teams in football are left to slug it out.
+What a great week of playoffs! Now we enter into the final conference challenges from which will emerge our Super Bowl contenders!
+</p>
+
+<p>
+The Packers did it again with their wily quarterback. Do not despair Dallas fans; they are a young team and should be back. BUt what a great game! The Steelers and Chiefs brought it down to the last second. All in all the best teams in football are left to slug it out.
 </p>
 
 <p>
 So the real fun continues. Wildcat Wonder has finished and uploaded the updated brackets. The new stats are updated and in. The format for next week is 2 games on Sunday.</p>
 
 <p>
-We had another great turnout this week. Thank you all again for participating. We have a 2 way tie as 18 Wheeler, and Toothfairy share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by Wildcat Wonder in second place and Exploding Brain in third. 
+We had another great turnout this week. Thank you all again for participating. We have a 2 way tie as 18 Wheeler, and Toothfairy share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by Wildcat Wonder in second place and NFL Amateur in third. 
 </p>
 
 <p>
@@ -28,7 +32,7 @@ Johor Tigress retaines command of first place of the <span style='font-weight:bo
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have 18 Wheeler, Super Brawler, The Fighting Ducks and Toothfairy. Check them out fllowing! Way to go folks! The stats are below. Read em and weep (I know I do)!
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have beach goer, DavidD, Johor Tigress, Lady Luck NFL Amateur, Scotsman357, Skinny, Steeel Got a Chance and Tenn Vols. Check them out following! Way to go folks! The stats are below. Read em and weep (I know I do)!
 </p>  
 ";
 ?>
