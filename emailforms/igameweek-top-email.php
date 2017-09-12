ec3655faf3f077d41402318f72d734120b4261d7
Tarrant Cutler <tarryc@daisy.westell.com>
2017-09-12 15:34:12 -0400

    updated gameweek top

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -3,31 +3,40 @@
 $emailtopwriteup = "
 <div style='padding-top:25px;font-size:16px; font-style: normal;color:black;'>
 
-Hi <span style='color:teal;'>All Daredevil Ducks members</span>,
+Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 
 <br />
 
 <p>
-What a great week of playoffs! Now we enter into the final conference challenges from which will emerge our Super Bowl contenders!
+A great first week of football. The Patriots came out in pitiful fashion and basically laid an egg. But  the season is very young. The Browns gave the Steelers better then I expected, but came out on the loosing end. The Lions won a competitive game against the Cardinals by coming on strong in the 4th quarter. The 
+Redskins lost at home to a good looking Eagles team. The Los Angeles Rams put a serious hurt on the lackluster Colts. Though Brisset looked good as quarterback in his short playing time at the end. The Jaguars ruined the Texan's home opener by pounding them at home. Looks like their problems weren't just due to Osweiler!
 </p>
 
 <p>
-The Packers did it again with their wily quarterback. Do not despair Dallas fans; they are a young team and should be back. BUt what a great game! The Steelers and Chiefs brought it down to the last second. All in all the best teams in football are left to slug it out.
+The Packers beat the Seahawks in a game that started out as a snoozer, but picked up momentum at the end. The Giants lost
+to the Cowboys and the Vikings looked very good in their win over the Saints. It took a late game field goal block for the 
+Broncos to salt the game away from the comeback minded Chargers. All in all a great first week.
+
+<p>
+Wildcat Wonder is working on the game stats, and we should have these available to you soon. 
+<br /><br />
+We added 10 expert football analysts. You will see them in our member lists for both picks and player stats. You can use these as you choose, like to help you pick or compare your scores. The daredevilducks community faired very well on balance against these experts.  
+<p>
+We had a great turnout this week. Thank you all again for participating. We have a 2 way tie as Merida and Smokey SouWester share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by a plethora of people in second and third. Check out 
+the numbers below.
 </p>
 
 <p>
-So the real fun continues. Wildcat Wonder has finished and uploaded the updated brackets. The new stats are updated and in. The format for next week is 2 games on Sunday.</p>
-
-<p>
-We had another great turnout this week. Thank you all again for participating. We have a 2 way tie as 18 Wheeler, and Toothfairy share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by Wildcat Wonder in second place and NFL Amateur in third. 
+Merida and Smokey SouWester also share command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. 
+ Again check out the numbers below!
 </p>
 
 <p>
-Johor Tigress retaines command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. She is followed by 18 Wheeler in second and Toothfairy in third.
-</p>
-
-<p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have beach goer, DavidD, Johor Tigress, Lady Luck NFL Amateur, Scotsman357, Skinny, Steeel Got a Chance and Tenn Vols. Check them out following! Way to go folks! The stats are below. Read em and weep (I know I do)!
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have (you guessed it) Merida and Smokey SouWester! Way to go folks! 
 </p>  
+
+<p>
+Don't despair to those who did not make the top tiers! It is a looooong season. And there WILL be great fluctuation in the leaders! The stats are below. Read em and weep (I know I do)!
+</p>   
 ";
 ?>
