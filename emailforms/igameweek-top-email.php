05ad9e932784551c648eb489c88eb43d8c431313
yellowrangler <tarrant.cutler@gmail.com>
2017-09-19 10:31:07 -0400

    Week 2 review
    Front page and email updates

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -8,35 +8,33 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 <br />
 
 <p>
-A great first week of football. The Patriots came out in pitiful fashion and basically laid an egg. But  the season is very young. The Browns gave the Steelers better then I expected, but came out on the loosing end. The Lions won a competitive game against the Cardinals by coming on strong in the 4th quarter. The 
-Redskins lost at home to a good looking Eagles team. The Los Angeles Rams put a serious hurt on the lackluster Colts. Though Brisset looked good as quarterback in his short playing time at the end. The Jaguars ruined the Texan's home opener by pounding them at home. Looks like their problems weren't just due to Osweiler!
+	Another great week of football. The Patriots came out their funk and put down the Saints in New Orleans. Question - What happens if you loose the first two games of the seanson and dont score a touchdown. Answer - you fire the offencsive coordinator. And that is exactly what the Bengals did after another stinker of a game against the Texans (who in spite of Deshaun Watson's moments of compenetnce still look like a second tier team). The Browns lost again this time to the Ravens. My heart goes out to the fans in Cleveland. The Buccaneers are definately a team to watch. They beat the straw out of the Bears who I though looked ok in the first week. The Steelers crushed the Bradford-less Vikings. 
+</p>
+<p>
+	There werer a number of close and entertaining games. The Chiefs continued their perfect record in a squeaker against the Eagles. The Dolphins and Chargers debuted Cutler in Miami. The game came down to another missed field goal by the Chargers rookie kicker. The Redskins avoided the dreaded 0 and 2 start for the season by beating the Eagles (there are very onerous statistics regarding teams that start 0 and 2. However, Mark Twain is quoted as saying that there are 'Lies, damned lies, and statistics').
+</p>
+<p>
+	Other games of note found the Broncos pounding Dallas in a game that was momentarily stopped due to lightning. The Falcons looked very impressive in their pummelling of the Packers. Lastly, the Giants were defeated at home by a balanced looking Lions. All in all a great Second week.
+</p>
+<p>
+	Wildcat Wonder is working on the game stats, and we should have these available to you soon. 
 </p>
 
 <p>
-The Packers beat the Seahawks in a game that started out as a snoozer, but picked up momentum at the end. The Giants lost
-to the Cowboys and the Vikings looked very good in their win over the Saints. It took a late game field goal block for the 
-Broncos to salt the game away from the comeback minded Chargers. All in all a great first week.
-</p>
-<p>
-Wildcat Wonder is working on the game stats, and we should have these available to you soon. 
-<br /><br />
-We added 10 expert football analysts. You will see them in our member lists for both picks and player stats. You can use these as you choose, like to help you pick or compare your scores. The daredevilducks community faired very well on balance against these experts.  
-</p>
-We had a great turnout this week. Thank you all again for participating. We have a 2 way tie as Merida and Smokey SouWester share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by a plethora of people in second and third. Check out 
-the numbers below.
+	We had a great turnout this week. Thank you all again for participating. We have a 2 way tie as Merida and NFL Amateur share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by a plethora of people in second and third. Check out the numbers below.
 </p>
 
 <p>
-Merida and Smokey SouWester also share command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. 
- Again check out the numbers below!
+	Merida and NFL Amateur also share command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. 
+ 	Again check out the numbers below!
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have (you guessed it) Merida and Smokey SouWester! Way to go folks! 
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have NFL Amateur! Way to go Jessica! 
 </p>  
 
 <p>
-Don't despair to those who did not make the top tiers! It is a looooong season. And there WILL be great fluctuation in the leaders! The stats are below. Read em and weep (I know I do)!
+Don't despair to those who did not make the top tiers! There is plenty pf football left. More then enough time and games to make a run at the top! Read em and weep (I know I do)!
 </p>   
 ";
 ?>
