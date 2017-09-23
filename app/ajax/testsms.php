2469d638c6b8e217bb518d3df57fcb9cd4210817
Tarrant Cutler <tarryc@daisy.home>
2017-09-23 18:32:50 -0400

    sms test overide parms

--- app/ajax/testsms.php
+++ app/ajax/testsms.php
@@ -1,6 +1,9 @@
 <?php
 require_once "Mail.php";
 
+ini_set('display_errors','on');
+error_reporting(E_ALL);
+
 $textNumber = "9782392650";
 $textCarrier = "messa­gin­g.s­pri­ntp­cs.com";
 $textTo = $textNumber . "@" . $textCarrier;
