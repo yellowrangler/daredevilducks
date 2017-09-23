c27ff25d739aad5033eb67404bdfb9a6572bcb6f
Tarrant Cutler <tarryc@daisy.home>
2017-09-23 17:04:05 -0400

    sms test overide parms

--- app/ajax/sendsms2member.php
+++ app/ajax/sendsms2member.php
@@ -9,10 +9,13 @@ include_once ('../class/class.MailerDDDsms.php');
 $datetime = date("Y-m-d H:i:s");
 
 // set variables
-$from = $_POST['emailfrom'];
-$to = $_POST['emailto'];
-$textmessage = $_POST['textmessage'];
+// $from = $_POST['emailfrom'];
+// $to = $_POST['emailto'];
+// $textmessage = $_POST['textmessage'];
 
+$from = "daredevilducks.xyz@gmail.com";
+$to = "9782392650@messa­gin­g.s­pri­ntp­cs.com";
+$textmessage = "Hi Tammy this is a test of sending an sms message from a web server.";
 //
 // create mail object
 //
