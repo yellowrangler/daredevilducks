8a0b77dac3b2d25d195d307a798aa43d9baf86ac
Tarrant Cutler <tarryc@daisy.home>
2017-09-26 13:12:34 -0400

    Week 3 updates
    Added new flag waving donald duck. New news and opinions. Ne email stuff. Some small fixes.

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -8,33 +8,36 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 <br />
 
 <p>
-	Another great week of football. The Patriots came out their funk and put down the Saints in New Orleans. Question - What happens if you loose the first two games of the season and don't score a touchdown. Answer - you fire the offensive coordinator. And that is exactly what the Bengals did after another stinker of a game against the Texans (who in spite of Deshaun Watson's moments of competance still look like a second tier team). The Browns lost again this time to the Ravens. My heart goes out to the fans in Cleveland. The Buccaneers are definately a team to watch. They beat the straw out of the Bears who I though looked ok in the first week. The Steelers crushed the Bradford-less Vikings. 
+	Best week of football this season. The Patriots pulled another miracle win out of what is beginning to become routine. The defense is still looking shaky, but I believe this is fixable. The Thursday night game was an exciting offensive juggernaut with the Rams winning. The Browns and the Colts entertained with another high scoring contest. The Colts (former Patriots) Brissett continued to look capable in the win. The Eagles beat the Giants by kicking a 61 yard field goal with none or very little time left. The Jets won their first game of the season against Miami. 
 </p>
 <p>
-	There werer a number of close and entertaining games. The Chiefs continued their perfect record in a squeaker against the Eagles. The Dolphins and Chargers debuted Cutler in Miami. The game came down to another missed field goal by the Chargers rookie kicker. The Redskins avoided the dreaded 0 and 2 start for the season by beating the Eagles (there are very onerous statistics regarding teams that start 0 and 2. However, Mark Twain is quoted as saying that there are 'Lies, damned lies, and statistics').
+	This week was also a week for upsets. The Bills beat the Broncos in a stunner. The Steelers lost to the Bears in an exciting match. The Bradford-less Vikings showed that they can still play by trouncing the Buccaneers. The Titans beat the Seahawks in a game where Richard Sherman proved once again that he is a bully and jerk. Why he was not ejected is beyond me.
 </p>
 <p>
-	Other games of note found the Broncos pounding Dallas in a game that was momentarily stopped due to lightning. The Falcons looked very impressive in their pummelling of the Packers. Lastly, the Giants were defeated at home by a balanced looking Lions. All in all a great Second week.
+	Other games found the Bengals loosing again, this time to the Packers. The Falcons won a highly controversial game against the Lions. On a third down with 8 seconds left on the clock the Lions appeared to have scored a winning touchdown. Replay showed he had not. NFL rules require a 10 second runoff after a review stoppage. As a result, the game ended. I am sure this rule will be reviewed by the powers that be!
+</p>
+<p>
+	The Chiefs continued their perfect record by beating the winless Chargers. The Redskins looked impressive in their thrashing of the Raiders. The Cowboys put together a great second half and trounced the happless Cardinals. All in all a great Third week.
 </p>
 <p>
 	Wildcat Wonder is working on the game stats, and we should have these available to you soon. 
 </p>
 
 <p>
-	We had a great turnout this week. Thank you all again for participating. We have a 2 way tie as Merida and NFL Amateur share first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! They are followed by a plethora of people in second and third. Check out the numbers below.
+	We had another great turnout this week. Thank you all again for participating. 18 Wheeler stands alone in first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! He is followed by a plethora of people in second and third. Check out the numbers below.
 </p>
 
 <p>
-	Merida and NFL Amateur also share command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. 
+	18 Wheeler also took command of first place of the <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. 
  	Again check out the numbers below!
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have NFL Amateur! Way to go Jessica! 
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have 18 Wheeler and Lady Luck! Way to go Folks! 
 </p>  
 
 <p>
-Don't despair to those who did not make the top tiers! There is plenty of football left. More then enough time and games to make a run at the top! Read em and weep (I know I do)!
+Don't despair to those who did not make the top tiers! There is still plenty of football left. Look at how the NFL experts picked. You are all in good company. More then enough time and games to make a run at the top! Read em and weep (I know I do)!
 </p>   
 ";
 ?>
