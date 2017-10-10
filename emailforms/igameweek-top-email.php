216290240612d8b17c35245e656381f27230da4a
yellowrangler <tarrant.cutler@gmail.com>
2017-10-10 16:18:59 -0400

    Week review
    Opinion and Review update
    email week end top update

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -8,10 +8,10 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 <br />
 
 <p>
-	Another week in this crazy season. The Patriots defence put in another stinker against the Panthers. Much will have to improve if we expect even a whiff of post season. The Saints pounded the hapless Dolphins. The Titan's in a week eerily reminiscent of last seasons final regular week found both Oakland and Titan's quarterbacks knocked out of the game. Speaking of the Titan's, they were pounded by the Texans. Deshaun Watson is beginning to look more and more like the real thing.
+	Another week in this crazy season. The Patriots defense put in another stinker against the Panthers. Much will have to improve if we expect even a whiff of post season. The Saints pounded the hapless Dolphins. The Titan's in a week eerily reminiscent of last seasons final regular week found both Oakland and Titan's quarterbacks knocked out of the game. Speaking of the Titan's, they were pounded by the Texans. Deshaun Watson is beginning to look more and more like the real thing.
 </p>
 <p>
-	The Jets beat the Jaguars in a close game. The Lions won a contested match against the much improved Vikings. The Steelers put the hammer on the Ravens. The Bengals finally won a game aginst the winless Browns. The Rams beat the Cowboys in a great offensive contest.  The Eagles dispatched the poor Chargers in a close game.
+	The Jets beat the Jaguars in a close game. The Lions won a contested match against the much improved Vikings. The Steelers put the hammer on the Ravens. The Bengals finally won a game against the winless Browns. The Rams beat the Cowboys in a great offensive contest.  The Eagles dispatched the poor Chargers in a close game.
 </p>
 <p>
 	This week had one big upset as the Bills beat the Falcons to take first place in their division. Is this the year for the Bills? The Giants lost again, this time to the Buccaneers. The Cardinals and Broncos eked eked out wins. The Seahawks destroyed the Colts and the Chiefs won a cliff hanger against the Redskins. All in all a great Third week.
@@ -22,7 +22,7 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 </p>
 
 <p>
-	We had another great turnout this week. Thank you all again for participating. 18 Wheeler continues to own first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! He is followed by a plethora of people in second and third. 
+	We had another great turnout this week. Thank you all again for participating. 18 Wheeler continues to own first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! But he had better watch out as Peanuts and Tooth Fairy are hot on his trail in second!
 </p>
 
 <p>
@@ -31,7 +31,7 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have Ginger in first place; followed by a number of others in second! Way to go Ginger! 
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have a trifecta as ANTIC, Hygenius and Kitten Mittens are all tied for first place; followed by a number of others in second! Way to go Folks! 
 </p>  
 
 <p>
