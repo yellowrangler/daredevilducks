588f10212d1b498e59cc2f0ebd5a9069a07dc0f0
yellowrangler <tarrant.cutler@gmail.com>
2017-10-24 19:58:02 -0400

    Week 7
    Opinion and news
    Email review
    2 new stats import files

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -8,31 +8,31 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 <br />
 
 <p>
-	Another week in a crazy season. The Patriots defense still needs work. The offence was able to put enough points on the board to beat the Jets. But an arcane rule spoiled the match. The Eagles beat the Panthers in a back and forth game. The Bears beat the Ravens in OT to keep their progress alive. The Browns lost again, this time to the Texans. The Packers lost to the Vikings and in the effort lost their star quarterback Aron Rogers. The Saints drilled the fickle Lions.
+	Another great week. The Patriots defense finally got some consistency as they kept the Atlanta team in check all night. The Pats also showed some running dominance which sealed the win in the home stretch. The Chiefs continued their slide with a loss to the Raiders in a nail biter. The Bills continued to show they belong by beating the Buccaneers in another thriller. The Vikings handily beat the Ravens.
 </p>
 <p>
-	Is it me, or does the league seem to be loosing top teams as mediocracy settles in. Last years super bowl contender Falcons lost to the aimless Dolphins. The Redskins squeaked by the struggling 49ers. The Buccaneers lost to the Cardinals, while the Rams beat the Jaguars. The Steelers and Big Ben showed their steel by beating the undefeated Chiefs. Don't count out the Steelers. When focused they are formidable!
+	The Jets lost to the home team Dolphins. The Cardinals were pummeled by the Rams in London. A game that also cost Carson Palmer possibly the rest of the season with a broken arm. The hapless Colts were soundly defeated by the surging Jaguars. The Saints beat the Rogers-less Packers and the Bears beat the Panthers.
 </p>
 <p>
-	The Chargers beat the Raiders, and the 5 and 0 Giants beat the Broncos at HOME. Really! Finally, the Titans beat the Colts in a badly played game. Still, all in all a great sixth week.
-</p>
-
-
-<p>
-	Wildcat Wonder is working on the game stats, and we should have these available to you soon. 
+	The Titans beat the Browns in a snoozer. The Cowboys lassoed the at home 49rs. And the Chargers branded the sliding Broncos. The Steelers got focused and took care of business, easily dispatching the Bengals. Here come the Steelers. Watch out! The Seahawks did what was expected of them and beat the Giants in NY. And in one of the more entertaining games, the Eagles out lasted the Redskins in Philly. As I like to say, all in all a great seventh week.
 </p>
 
+
 <p>
-	We had another great turnout this week. Thank you all again for participating. 18 Wheeler continues to own first place on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! But he has his grand daughter NFL Amateur hot on his tail!
+	Wildcat Wonder has finished the game statsand these are now available to you! 
 </p>
 
 <p>
-	18 Wheeler also continues command of first place of <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. 
+	We had another great turnout this week. Thank you all again for participating. 18 Wheeler now shares first place with his Grand Gaughter NFL Amateur on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! But they have Wildcat Wonder (My Wife and 18 Wheeler's daughter) hot on their tail in second! And to complete the Jamieson/Hunzelman trifecta we have Toothfairy (NFL Amateur's sister and 18 Wheeler's Grand Daughter AND Wildcat Wonder's niece) in third! What a family.
+</p>
+
+<p>
+	18 Wheeler and NFL Amateur have command of first place of <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. They are followed by Wildcat Wonder in second and Tooth Fairy in third.
  	Check out the numbers below!
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we Dr Joy in first place; followed by a Blue Lioness in second! Way to go Folks! 
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have Johor Tigress, NFL Amateur, PatrickR and Wildcat Wonder in first place; followed by many in second and third! Way to go Folks! 
 </p>  
 
 <p>
