4b1a64a1e51eb7ec8bc6d2da29f01646db993c1b
yellowrangler <tarrant.cutler@gmail.com>
2017-10-31 18:52:44 -0400

    Week 8 email and stats
    Week 8 email and stats

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -3,37 +3,33 @@
 $emailtopwriteup = "
 <div style='padding-top:25px;font-size:16px; font-style: normal;color:black;'>
 
-Hi <span style='color:teal;'>Daredevil Ducks members</span>,
+Hi <span style='color:teal;'>Daredevil Ducks members</span> and <span style='font-weight:bold; color:#FF9100;'>All you who will be Ghosts, Vampires and Princesses Tonight!</span>,
 
 <br />
 
 <p>
-	Another great week. The Patriots defense finally got some consistency as they kept the Atlanta team in check all night. The Pats also showed some running dominance which sealed the win in the home stretch. The Chiefs continued their slide with a loss to the Raiders in a nail biter. The Bills continued to show they belong by beating the Buccaneers in another thriller. The Vikings handily beat the Ravens.
+	Another crazy week. The Patriots hung on to beat the Chargers in Foxborough. The Dolphins got their butts powdered by the Ravens. The Browns continued their winless skid loosing this time to the Vikings. The equally winless 49ers lost to the Eagles. The Falcons beat the Jets in a close match under monsoon conditions. The Panthers looked good in their win over the faultering Buccaneers.
 </p>
 <p>
-	The Jets lost to the home team Dolphins. The Cardinals were pummeled by the Rams in London. A game that also cost Carson Palmer possibly the rest of the season with a broken arm. The hapless Colts were soundly defeated by the surging Jaguars. The Saints beat the Rogers-less Packers and the Bears beat the Panthers.
-</p>
-<p>
-	The Titans beat the Browns in a snoozer. The Cowboys lassoed the at home 49rs. And the Chargers branded the sliding Broncos. The Steelers got focused and took care of business, easily dispatching the Bengals. Here come the Steelers. Watch out! The Seahawks did what was expected of them and beat the Giants in NY. And in one of the more entertaining games, the Eagles out lasted the Redskins in Philly. As I like to say, all in all a great seventh week.
-</p>
-
-
-<p>
-	Wildcat Wonder has finished the game statsand these are now available to you! 
+	The resurgent Saints beat the Bears. The Bills are beginiing to look like a contender as they easily dispatched the Raiders for a win. The Bengals did win against the Colts, but it was a comedy of errors on both sides. The Seahawks beat the Texans in best game of the week. The Cowboys beat the struggling Redskins. The Steelers continued their winning ways with a victory over the Lions. The Chiefs exposed the Broncos offense with a decisive win.  
 </p>
 
 <p>
-	We had another great turnout this week. Thank you all again for participating. 18 Wheeler now shares first place with his Grand Gaughter NFL Amateur on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! But they have Wildcat Wonder (My Wife and 18 Wheeler's daughter) hot on their tail in second! And to complete the Jamieson/Hunzelman trifecta we have Toothfairy (NFL Amateur's sister and 18 Wheeler's Grand Daughter AND Wildcat Wonder's niece) in third! What a family.
+	We had another great turnout this week. Thank you all again for participating. 18 Wheeler continues to share first place with his Grand Daughter NFL Amateur on the <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! But they still have Wildcat Wonder on their tail in second! Merida, Peanuts, The Fighting Ducks and Toothfairy in third! Yowsa!
 </p>
 
 <p>
-	18 Wheeler and NFL Amateur have command of first place of <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. They are followed by Wildcat Wonder in second and Tooth Fairy in third.
+	18 Wheeler and NFL Amateur have command of first place of <span style='font-weight:bold; color:blue;'>Total Percentage Leader</span> Board. They are followed by Wildcat Wonder in second with Merida, Peanuts, The Fighting Ducks and Toothfairy in third.
  	Check out the numbers below!
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have Johor Tigress, NFL Amateur, PatrickR and Wildcat Wonder in first place; followed by many in second and third! Way to go Folks! 
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have Merida and Scotsman357 in first place. They did the hard way they EARNED it by picking a perferct 13 Wins and 0 Losses! They are followed by many in second and third! Way to go Folks! 
 </p>  
+
+<p>
+	The Statistics for week 8 are available! 
+</p>
 
 <p>
 Don't despair to those who did not make the top tiers! There is still plenty of football left. Look at how the NFL experts picked. You are all in good company. More then enough time and games to make a run at the top! Read em and weep (I know I do)!
