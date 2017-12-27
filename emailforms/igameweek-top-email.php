b74ac204d08e478a149bae7de8bfa8dc1f47d17a
yellowrangler <tarrant.cutler@gmail.com>
2017-12-27 18:00:08 -0500

    Week 16
    Opinion and brackets. Modal larger

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -8,24 +8,21 @@ Hi <span style='color:teal;'>Daredevil Ducks members</span>,
 <br />
 
 <p>
-	Only 2 more weeks to go! The Patriots turned their last weeks disater into this weeks gem. This was one of the great games of the year. Either team could have taken this. This game had everything. The Steelers scoring late in the fourth quarter, followed by the Patriots behind Brady and Gronk seemingly scoring the wining touchdown, followed by the tremendous catch and run by the Steelers to the 10 yard line, followed by the Jessie James touchdown that has called back (and which will fuel debate on the definition of a catch for the off season rule makers. Tuck rule anyone?), followed by the interception and final score by the Patriots defense. And all of this in the last 3 or so minutes. Great game. It will behoove the football gods to have these two teams meet again in the playoffs. 
+	We are down to the final week! Where has the time gone! The Patriots started out slow but finally took it to the Bills defeating them handily keeping their 1st seed position in the AFC intact. The Ravens beat the Colts keeping them active in the wildcard hunt. The Vikings defeated the Packers solidifying their playoff spot as 2nd seed in the NFC. The Bengals beat the going, going gone Lions. The Chiefs beat the Dolphins positioning the Chiefs as 4th seed AFC. The Browns lost again; this time to the Bears.
 </p>
 <p>
-	Now the rest of the games! The Broncos beat the Colts in a ho hum affair. The Lions beat the Bears keeping their playoff hopes alive. The Chiefs crushed the Chargers showing that they are the hardest team to pick/ignore! The Ravens beat the hapless Browns keeping their hopes for the post season active. The Redskins beat the Cardinals in a game that means nothing other then pride. The Eagles beat the Giants but not decisively, creating some to speculate that the Eagles run may be over. The Bills beat the Dolphins keeping their playoff hope alive. The Packers with Rodges lost a heart breaker to the Panthers. The Panthers continue to impress.
-</p>
-<p>
-	The Jaguars bludgeoned the Texans and the Vikings did the same to the Bengals. The Saints beat the Jets to stay playoff active, and the Rams embarrassed the Seahawks. First game Russel Wilson was unable to dance his way out of trouble. A true statement game for the Rams. The 49er's beat the Titans in a close match. Remarkably, the Titan's are still in contention for the playoffs. The Cowboys beat the Raiders, but looked bad doing it. The outcome was in doubt until Derek Carr fumbled the ball through the end zone, sealing the win for the Cowboys. The Falcons outlasted the Buccaneers in another ugly game. 
+	The Panthers beat the Buccaneers keeping them ensconced in the Wild card hunt. The Saints beat the Falcons in a game where the Falcon's looked awful. The Redskin's crushed the Broncos. The Rams squeaked by the Titan's and the Chargers beat the Jet's. In a surprise the 49ers beat the Jaguars. And the Seahawks beat the Cowboys; bouncing the Cowboys from the playoffs and revitalizing the Seahawks chances. The Cardinals bludgeoned the Giants and the Steelers beat the Texans. In the final game the Eagles held on to beat the Raiders, preserving the Eagles first seed in the NFC.
 </p>
 <p>
 	We had another great turnout this week. Thank you all again for participating. Hawk Eye continues to keep sole possession of first place for <span style='font-weight:bold; color:maroon;'>Total Games Won Leader</span> board! She is followed by NFL Amateur in second with Wildcat Wonder in third.
 </p>
 
 <p>
-For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have 18 Wheeler, Clammer, Empty Nesters, Johor Tigress and Skinny in first place. They are followed by a large group of folks in second and third! Way to go Folks! 
+For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> bragging rights we have 18 Wheeler, Island Girl and PatrickR in first place. They are followed by a large group of folks in second and third! Way to go Folks! 
 </p>  
 
 <p>
-	The Statistics for week 15 are ready! 
+	The Statistics for week 18 are ready! 
 </p>
 
 <p>
@@ -33,7 +30,7 @@ For <span style='font-weight:bold; color:#228b22;'>Player Weekly Totals</span> b
 </p>
 
 <p>
-Time to get your MoJo on for those who are not in the top tiers! There are only a few weeks of football left. The stats are bellow! Read em and weep (I know I do)!
+Time to get your MoJo on for those who are not in the top tiers! There is only one week of regular season football left. The stats are below! Read em and weep (I know I do)!
 </p>   
 ";
 ?>
