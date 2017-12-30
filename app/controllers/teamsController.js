3ae840ffe14f18b84de41724263b8980c81c4dd9
yellowrangler <tarrant.cutler@gmail.com>
2017-12-30 17:18:10 -0500

    Add new Brackets
    Add images

--- app/controllers/teamsController.js
+++ app/controllers/teamsController.js
@@ -220,7 +220,7 @@ controllers.playoffstandingsController = function ($scope, $http, $location, nfl
                 break;
 
             case "2017":
-                $scope.bracketimg = "NFLPlayOffBracketSuperbowl52-2018A.png";
+                $scope.bracketimg = "NFLPlayOffBracketSuperbowl522018A2.png";
                 break;                
 
             default:
--- mobile/app/controllers/teamsController.js
+++ mobile/app/controllers/teamsController.js
@@ -1472,7 +1472,7 @@ controllers.playoffstandingsController = function ($scope, $http, $location, nfl
                 break;
 
             case "2017":
-                $scope.bracketimg = "NFLPlayOffBracketSuperbowl52-2018A.png";
+                $scope.bracketimg = "NFLPlayOffBracketSuperbowl522018A2.png";
                 break;     
 
             default:
