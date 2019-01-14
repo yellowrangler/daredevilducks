1ab5c7b8d7f197ffb41853f642f6fc17d9d9568d
tarryc <tarrant.cutler@gmail.com>
2019-01-14 17:02:40 -0500

    week 20 stats and brackets

--- emailforms/igameweek-bottom-email.php
+++ emailforms/igameweek-bottom-email.php
@@ -5,6 +5,6 @@ $emailbottomwriteup = "
 We hope all members had a Happy New Year!
 
 <br /><br /><br />
-There is NO Thursday Night Game and NO Monday Night Game.
+There are only Sunday Games.
 ";
 ?>
--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -5,16 +5,19 @@ $emailtopwriteup = "
 The Playoff are here and what a great regular season we had. This is our favorite time of the year. Savor it as you watch it because it will be over in the blink of an eye.
 </p>
 <p>	
-The Colts crushed the Texans in the first contest of the playoffs. The much vaulted Texan defense was unable to stop Andrew Luck as the Colts put all the points that they needed up in the first half. The anemic and over hyped Texas offense led by Watson manged only 7 points. Watch out for the Colts!
+The Chiefs crushed the Colts at the frozen Arrow Head field in Kansas City! It is very hard to see who can beat this offensive juggernaut. The Chiefs seem capable of scoring however many points are necessary to win. Their next opponent will be the New England Patriots for the AFC championship!
 </p>
 <p>	
-The Cowboys beat a good Seahaks team. Neither team looked invincible, as the game was at times a bit of a snooze. But a win gets you another day. The Cowboys will have to play very well to continue to the Super bowl.
+The Rams beat the Cowboys by exerting a familiar and regular pattern of running and passing. Dallas did there best to stay even, but in they end were overwhelmed. The Rams next meet the New Orleans Saints for the NFC championship!
 </p>
 <p>	
-The Chargers beat the Ravens in a great game. It was a hard hitting contest with the difference being each teams quarterback. Both teams played a solid defense. In the end, Jackson just was too one dimensional and could not make the required plays. While I stand with the Patriots, there is something eminently likable about Phil Rivers and this entire Charger team. They play the Patriots next which will be there greatest test to date.
+The Patriots stuck it to all the naysayers and crushed the Chargers in a game where both the offense and defense performed flawlessly. Their next opponent will be the Kansan City Chiefs for the AFC championship!
 </p>
 <p>	
-The Eagles beat the Bears in the craziest game of the week. This was a game where both teams played well. The game swung back and forth. Foles brought the Eagles down and scored putting the Eagles in the lead with seemingly very little time on the clock; only to have Mitch Trubisky bring the Bears to within field goal range (43 yards) and the win. The Bears kicker hit both the upright and the horizontal bar eventually bouncing forward back out onto the field. It was a tough loss for the Bears, and another part of the Philadelphia miracle as they continue to just win.
+The Saints beat the Eagles in the most closely contested playoff to date. Only a late fourth quarter interception sealed the win for the Saints. The Saints next meet the Los Angeles Rams for the NFC championship!
+</p>
+<p>	
+So there we have it. The number 1 and 2 teams from their respective conferences will duke it out for a chance to play in the Super Bowl. Seems like poetic symmetry.
 </p>
 <p>
   All in all a great week of football!
@@ -30,16 +33,17 @@ The Eagles beat the Bears in the craziest game of the week. This was a game wher
 
 <p>
 	Wildcat Wonder added a new chart which shows and will continue to show; where teams stand with respect to the playoffs. Including 1st round byes, Wild Cards and 'In the Hunt'. You will find this new chart on the 'Team Playoff Brackets' page under 'Teams' AND by clicking on the large Daredevil ducks icon on the home page.
-</p> 
-
-<!-- <p>
-	The Statistics from week 15 will be available soon!
 </p>
- -->
+
+<p>
+	The Statistics and playoff bracket for week 19 will be available soon!
+</p>
+
+<!-- 
 <p>
 	The Statistics from week 19 are available thanks to the hard work of Wildcat Wonder!
 </p>
-
+ --> 
 
 
 <p>
