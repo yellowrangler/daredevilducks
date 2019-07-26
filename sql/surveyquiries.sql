e633431163f1580d01efeb999171d40681cf303b
Yellowrangler <tarrant.cutler@gmail.com>
2019-07-26 14:30:18 -0400

    add surveyquiries

---
+++ sql/surveyquiries.sql
@@ -0,0 +1,24 @@
+
+select count(distinct memberid) 
+from surveymemberanswerstbl SA;
+
+select distinct memberid, membername 
+from surveymemberanswerstbl SA
+left join membertbl M on M.id = SA.memberid;
+
+select distinct memberid, membername, 
+(select count(surveyquestionid) 
+from surveymemberanswerstbl 
+where memberid = SA.memberid) as questionsanswered
+from surveymemberanswerstbl SA
+left join membertbl M on M.id = SA.memberid;
+
+select distinct memberid, membername
+from surveymemberanswerstbl SA
+left join membertbl M on M.id = SA.memberid
+where 
+(select count(surveyquestionid) 
+from surveyquestiontbl) = 
+(select count(surveyquestionid) 
+from surveymemberanswerstbl 
+where memberid = SA.memberid);
