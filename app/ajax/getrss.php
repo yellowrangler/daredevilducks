b0904d89c7cbb6d2a1b2ad4903bd2246f98181b5
Yellowrangler <tarrant.cutler@gmail.com>
2019-08-28 11:29:18 -0400

    rss issues debug code2

--- app/ajax/getrss.php
+++ app/ajax/getrss.php
@@ -30,6 +30,34 @@ else
 	}
 }
 
+function display_xml_error($error, $xml)
+{
+    $return  = $xml[$error->line - 1] . "\n";
+    $return .= str_repeat('-', $error->column) . "^\n";
+
+    switch ($error->level) {
+        case LIBXML_ERR_WARNING:
+            $return .= "Warning $error->code: ";
+            break;
+         case LIBXML_ERR_ERROR:
+            $return .= "Error $error->code: ";
+            break;
+        case LIBXML_ERR_FATAL:
+            $return .= "Fatal Error $error->code: ";
+            break;
+    }
+
+    $return .= trim($error->message) .
+               "\n  Line: $error->line" .
+               "\n  Column: $error->column";
+
+    if ($error->file) {
+        $return .= "\n  File: $error->file";
+    }
+
+    return "$return\n\n--------------------------------------------\n\n";
+}
+
 //
 // get date time for this transaction
 //
@@ -41,15 +69,19 @@ try {
   $rssContent = simpleXML_load_file($url, 'SimpleXMLElement',LIBXML_NOCDATA);
   if ($rssContent === false)
   {
-    foreach(libxml_get_errors() as $error) {
-        echo "\t", $error->message;
+  	$errors = libxml_get_errors();
+    foreach($errors as $error) {
+        // echo "\t", $error->message;
+        echo display_xml_error($error, $xml);
     }
+
+    libxml_clear_errors();
   }
 } catch (Exception $e) {
     echo "/n Exception /n";
 }
 
-var_dump ( libxml_get_errors () );
+// var_dump ( libxml_get_errors () );
 $jsonContent = json_encode($rssContent);
 exit ($jsonContent);
 ?>
