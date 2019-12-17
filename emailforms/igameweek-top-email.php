bf2e580ae3fe4b9f9c7d5dc2f29dc459fa00aad6
Tarrant Cutler <tarrant.cutler@gmail.com>
2019-12-17 17:48:20 -0500

    week 15-16

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,23 +2,23 @@
 
 $emailtopwriteup = "	
 <p>
-The 14th week of the 2019 season is in the bag. The Daredevil Ducks rankings still continue to churn!
+The 15th week of the 2019 season is in the bag. The Daredevil Ducks rankings still continue to churn!
 </p> 
 
 <p>
-Winter is officially here (snow - not the December Solstice), Christmas continues to surge relentlessly towards us! And in the midst of it all is football!
+Christmas is right around the corner. Time to bring family and friends together to celebrate the birth of Christ. Time also for these darn referees to start getting their collective act together and start making consistent calls! God bless all our members and let us pray for Peace on Earth! On to football!
 </p> 
 
 <p>
-In the first match of the week, the Bears beat the stumbling 6 and 7 Cowboys. Strangely enough, the Cowboys can still win their Division and get a bye for the first round of the playoffs - if they beat the Eagles later in the season. The Falcons beat the plummeting Panthers. The Ravens beat the Bills in a thriller. The Browns just managed to squeak past the Bengals. The Packers beat the Redskins.
+The Ravens continued there relentless carnage on the NFL as they punished the Jets. The Seahawks beat the Panthers to tie the 49ers for first place in their division. The Patriots beat the hapless Bengals in Foxborough. The Buccaneers beat the Lions to even their record. The Packers beat the Bears.
 </p> 
 
 <p>
-In an upset of sorts, the Broncos crushed the Texans. The Vikings plundered the anemic Lions. In the game of the week, the 49ers beat the Saints with a last second field goal. The Jets beat the Dolphins, and the Buccaneers beat the Colts. The Chargers found their game as they pummeled the Jaguars. The Steelers beat the Cardinals. In a game marred by bad officiating, the Chiefs beat the Patriots in Foxborough.
+The Chiefs beat the Broncos is a snow-globe. The Giants beat the Dolphins. The Bills beat the Steelers in Pittsburgh. The Bills are for real! The Texans beat the Titans in a close game marred by more bad refereeing. The Eagles beat the Redskins to stay alive in the divisional hunt. The Cardinals clipped the mercurial Browns. In their last game in Oakland, the Raiders lost to the Jaguars. The Cowboys finally looked like the team they have been hyped to be - as they crushed the Rams. In an upset, the Falcons beat the 49ers. The Vikings beat the disappearing Chargers.
 </p> 
 
 <p>
-The rejuvenated Titans torched the Raiders. The Rams showed grit as they beat the Seahawks. And in the final game of the week, the Eagles beat the Giants in overtime. 
+And in the final game of the week, the Saints put a whooping on the fading Colts. 
 </p>
 
 <p>
@@ -27,7 +27,7 @@ The rejuvenated Titans torched the Raiders. The Rams showed grit as they beat th
 
 
 <p>
-There is still time to make a run for some, but the noose is closing! Get your strategies under control. Time for the cream to rise to the top! You get the point - Do your JOB!
+There is very little time to make a run for some, but the noose is closing! Get your strategies under control. Time for the cream to rise to the top! You get the point - Do your JOB!
 </p> 
 
 
