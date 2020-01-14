4127e83c7eaf6f17bf3c96f1cfaab4489c3c7329
Tarrant Cutler <tarrant.cutler@gmail.com>
2020-01-14 13:47:28 -0500

    week 20

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,27 +2,27 @@
 
 $emailtopwriteup = "	
 <p>
-The Wild Card Weekend is over! On to the Divisional Playoffs!
+The Divisional Playoff Weekend is over! On to the Conference Playoffs!
 </p> 
 
 <p>
-I hope you all had a Happy New Year! This week is the round 2 Divisional Playoffs (week 19). Lets review what happened last week.
+This week is the round 3 Conference Playoffs (week 20). Lets review what happened last week.
 </p> 
 
 <p>
-In the first Saturday game the Texans won in a real nail biter over the impressive Bills. Deshaun Watson looked every part the franchise player, as he took control when it counted. Josh Allen showed remarkable skills but made some classic mistakes at crunch time. A great game none the less! 
+In the first Saturday game the 49ers won over the Vikings. The game started out close, but San Fransisco proved too good as they ran away in the second half. 
 </p> 
 
 <p>
-In the second Saturday game, the Titans, behind the punishing running of Henry,  rolled over the hapless Patriots. The Patriots did shut Henry down in the second half, but were unable to take advantage of a first and goal which resulted in a field goal. Now Patriots nation is left to ponder the state of the team moving forward.
+In the second Saturday game, the Titans, behind the punishing running of Henry, beat the vaulted Ravens. It was a game hearkening back to the good old days of football where a balance of running and passing won the day. In the end the raw talent of Lamar could not overcome the more steady and disciplined Titans defense! 
 </p> 
 
 <p>
-In the first Sunday game, the Vikings pulled an upset of sorts by beating the Saints in New Orleans in Overtime. Cousins looked poised and was on target as he showed he could win a playoff game. The Vikings defense did the rest. Pressing the Saints offense into 2 turnovers. 
+In the first Sunday game, the Chiefs crushed the Texans in a game where the Texans scored the first 21 points in a seeming runaway. Only to have the Chiefs come fighting back and then some! Very impressive game for Chief fans.
 </p> 
 
 <p>
-In the second Sunday game, the Seahawks beat the Eagles. The Eagles lost their starting quarterback when his head was slammed into the ground during a tackle. No call by the refs. Damn if I can figure out what constitutes a penalty any more. 
+In the second Sunday game, the Packers beat the Seahawks. This was a close game that featured some big plays in the weaning minutes of the game by the Packers to keep the Seahawks offense off the field. 
 </p> 
 
 <p>
@@ -30,11 +30,11 @@ In the second Sunday game, the Seahawks beat the Eagles. The Eagles lost their s
 </p> 
 
 <p>
-It is now a small fraternity of those who can be a Daredevil ducks champion. To those few, pick wisely as the number of games is coming to a dramatic close. Good luck to all! To the rest of us (including myself), enjoy the sprint to the finish!
+It is now a very small fraternity of those who can be a Daredevil ducks champion. To those few, pick wisely as the number of games is coming to a dramatic close. Good luck to all! To the rest of us (including myself), enjoy the sprint to the finish!
 </p> 
 
 <p>
-<span style='color:#FF6E3B;font-weight:700;'>Click on the New Years Image on the home page and get a New Year message!</span>
+<span style='color:#FF6E3B;font-weight:700;'>Click on the Winter Image on the home page and get a New Year message!</span>
 </p>
 
 <p>
