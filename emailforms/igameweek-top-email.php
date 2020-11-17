b876a113b047bf25113d2e0db3b114bf84bdc342
Tarrant Cutler <tarrant.cutler@gmail.com>
2020-11-17 01:19:38 -0500

    season 2020 week 10

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,20 +2,16 @@
 
 $emailtopwriteup = "	
 <p>
-End to the ninth week of the Season!
-</p> 
-
-<p>
 Let us all take the time to pause and be Thankful for all that we have!
 </p>
 
 <p>
-In the first game, the Packers beat the 49ers. The Falcons beat the Broncos. The Bills in a bit of an upset, beat the Seahawks. The Ravens beat the Colts. The Texans beat the Jaguars and the Chiefs beat the Panthers. The Vikings crushed the Lions and the Titans beat the Bears.
+In the first game, the Colts beat the Titans in a back and forth affair. The Tom Brady Buccaneers got back on the winning track by pummeling the Panthers. The Browns beat the Texans, the Lions beat the Washingtons and the Packers beat the Jaguars. The Giants beat the flightless Eagles and the Cardinals beat the Bills.
 </p>
 
 
 <p>
-The Giants beat the Washingtons. The Raiders beat the fading Chargers. The Dolphins squeaked by the Cardinals while the Steelers continued their undefeated ways by beating the Cowboys in what only can be characterized as an ugly win. But a win is a win. The Tom Brady Buccaneers had their asses kicked by the Saints. And in the final game, the Patriots edged by the winless Jets.
+The Raiders beat the Broncos. The Dolphins continued to look good beating the Chargers. The Steelers continued their quest for an unbeaten season by beating the Bengals. The Rams gave the Seahawks their second loss in a row. The Saints beat the fading 49ers. The Patriots beat the Ravens in a monsoon. In the final game the Vikings beat the Bears.
 </p>
 
 <p>
