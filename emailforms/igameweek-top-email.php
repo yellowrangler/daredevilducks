a8647b9de2dc31a149a38af3bafe41cbc83e0a35
Tarrant Cutler <tarrant.cutler@gmail.com>
2020-12-02 22:06:57 -0500

    season 2020 week 12 no stats added christmas images for desktop and mobile

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,16 +2,20 @@
 
 $emailtopwriteup = "	
 <p>
-Let us all take the time to pause and be Thankful for all that we have!
+This was a curious week indeed. The Broncos were made to play without a quarterback, yet the Steelers and Ravens were permitted to push their match not once but three times to suit their covid issues! Doesn't seem like a level application of fairness and balance to me. What do you out there think? Remember the old days when you had a schedule and you were required to field the game with what players you had? Regardless of snow, rain, aliens or covid? Roger that!
 </p>
 
 <p>
-In the first game, the Seahawks won a close match against the Cardinals. The Titans beat the Ravens. The Panthers beat the Lions. The Browns won over the Eagles, and the Patriots lost a heart breaker with the Texans. No playoffs for you! The Colts in a bit of an  upset, beat the Packers.
+Our clearly incompetent IT staff did not envision Tuesday or Wednesday games. As a result, any games on those days will appear as very late Monday night games. So make sure you get those games selected on time. 
+</p>
+
+<p>
+In the first game, the Lions lost to the Texans and in so doing lost Patricia's job as head coach. The Washingtons beat the beastly Cowboys. The inconsistent Falcons beat the equally inconsistent Raiders. The Bills beat the Chargers, the Giants beat the Bengals and the Titans beat the Colts. The Browns beat the Jaguars. The Vikings won a much needed victory against the Panthers. The Patriots looked pathetic in their win over the Cardinals.
 </p>
 
 
 <p>
-The Steelers stayed undefeated as they easily dispatched the Jaguars. The Saints beat the Falcons, the Washingtons beat the Bengals and the Chargers beat the winless Jets. Denver beat the Dolphins. Dallas beat the disappointing Vikings. The Chiefs behind Mahome, came back from behind to beat the Raiders. In the final game, the Rams beat the Tom Brady Buccaneers.
+The Dolphins beat the still winless Jets. The Saints without their starting quarterback beat the Broncos without any quarterback! The 49ers won against the Rams. The Tom Brady Buccaneers lost to the Chiefs. The Packers beat the Bears, and the Seahawks beat the Eagles in what normally would have been the last game of the week! But wait, it's  a Christmas miracle! The Steelers and Ravens were rescheduled to play a new Wednesday night - er I mean afternoon (really?) game in which the Steelers won; remaining the only undefeated team. 
 </p>
 
 <p>
