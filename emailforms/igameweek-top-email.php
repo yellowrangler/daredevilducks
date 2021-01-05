e83088994fb42106bfcf3f3314c51c1b2ed365ec
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-01-05 12:44:47 -0500

    season=2020 week=17-18, no stats. corrected mistakes.

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,11 +2,11 @@
 
 $emailtopwriteup = "	
 <p>
-In the first game, the Bills secured a division title as they beat the Dolphins. The Ravens beat the Bengals. The Browns ended the longest playoff drought of any team by beatung the Steelers. The Vikings squeeked by the Lions. The Patriots beat the Jets; who in turn fired their head coach. The Giants beat the Cowboys. The Tom Brady Buccaneers beat the Falcons. The Saints beat the Panthers, the Packsres beat the Bears and the Titans beat the Texans in a thriller. 
+In the first game, the Bills secured a division title as they beat the Dolphins. The Ravens beat the Bengals. The Browns ended the longest playoff drought of any team by beating the Steelers. The Vikings squeaked by the Lions. The Patriots beat the Jets; who in turn fired their head coach. The Giants beat the Cowboys. The Tom Brady Buccaneers beat the Falcons. The Saints beat the Panthers, the Packers beat the Bears and the Titans beat the Texans in a thriller. 
 </p>
 
 <p>
-The Colts beat the Jaguars. The Chargers beat the Chiefs. The Raiders beat the Broncos. The Rams beat the Cardinals and the Seahawks beat the 49ers. In the most peculiar game of the week, the Washingtons beat the Eagles. This was in a game where the Eagles had a chance to win and put in their third string quarter back, esentially conceding the game to the Washingtons. Had the Washingtons not won the Giants would have taken the division title. Vvery strange. But what the hell, what hasnt been in 20201 (yea it was played in 2021, but it is technically the 2020 season.)
+The Colts beat the Jaguars. The Chargers beat the Chiefs. The Raiders beat the Broncos. The Rams beat the Cardinals and the Seahawks beat the 49ers. In the most peculiar game of the week, the Washingtons beat the Eagles. This was in a game where the Eagles had a chance to win and put in their third string quarterback, essentially conceding the game to the Washingtons. Had the Washingtons not won, the Giants would have taken the division title. Vvery strange. But what the hell, what hasn't been in 20201 (yes it was played in 2021, but it is technically the 2020 season.)
 </p>
 
 <p>
@@ -19,6 +19,10 @@ This officially ends the regular season. To be honest there has been nothing 'Re
 
 <p>
 But now the number of games diminishes and the winning picks become more elusive. So picking becomes more critical. Good look to the few that still have a chance to reign supreme at the end of the Season! 
+</p>
+
+<p>
+We will be adding playoff games as soon as the date and time become available!
 </p>
 
 
