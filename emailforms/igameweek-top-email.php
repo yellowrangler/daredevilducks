8301b86298ebc70badddd39da64a43d7fdf55c08
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-01-14 22:12:17 -0500

    season 2020 week 19 stats, bracket and news

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,11 +2,7 @@
 
 $emailtopwriteup = "	
 <p>
-In the first game, the Bills secured a division title as they beat the Dolphins. The Ravens beat the Bengals. The Browns ended the longest playoff drought of any team by beating the Steelers. The Vikings squeaked by the Lions. The Patriots beat the Jets; who in turn fired their head coach. The Giants beat the Cowboys. The Tom Brady Buccaneers beat the Falcons. The Saints beat the Panthers, the Packers beat the Bears and the Titans beat the Texans in a thriller. 
-</p>
-
-<p>
-The Colts beat the Jaguars. The Chargers beat the Chiefs. The Raiders beat the Broncos. The Rams beat the Cardinals and the Seahawks beat the 49ers. In the most peculiar game of the week, the Washingtons beat the Eagles. This was in a game where the Eagles had a chance to win and put in their third string quarterback, essentially conceding the game to the Washingtons. Had the Washingtons not won, the Giants would have taken the division title. Vvery strange. But what the hell, what hasn't been in 2020! (yes it was played in 2021, but it is technically the 2020 season.)
+In the first game, the Bills beat the Colts pushing their magical seson to the next game. Then the Rams beat the Seahawks in Seattle. The Tom Bradey Buccaaneers beat the Washingtons, keeping their playoff run allive. The Ravens beat the Titans in a close game. The Saints crushed the Bears. In the upset of Wild Card weekend, the heavily favored Steelers were beaten by the resiliant Browns!
 </p>
 
 <p>
@@ -14,15 +10,15 @@ The Colts beat the Jaguars. The Chargers beat the Chiefs. The Raiders beat the B
 </p>
 
 <p>
-This officially ends the regular season. To be honest there has been nothing 'Regular' about it! In the playoffs its one and done. So all teams will be giving it their all! So hold on to your seats as the fun really starts! On to Wild Card Weekend!
+This officially ends the Wild Card Weekend. On to Divisionsal Supremacy Weekend!
 </p>
 
 <p>
-But now the number of games diminishes and the winning picks become more elusive. So picking becomes more critical. Good look to the few that still have a chance to reign supreme at the end of the Season! 
+Games are continuing to diminish and the winning picks are becoming more elusive. So picking becomes more critical. Good look to the few that still have a chance to reign supreme at the end of the Season! 
 </p>
 
 <p>
-We will be adding playoff games as soon as the date and time become available!
+We add playoff games as soon as the date and time become available!
 </p>
 
 
