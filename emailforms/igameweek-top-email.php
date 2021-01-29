3f63a4432ce510104d95b3c8f605197b01959c5c
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-01-28 20:53:12 -0500

    season = 2020 week = 20 no stats no bracket

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,11 +2,15 @@
 
 $emailtopwriteup = "	
 <p>
-In the first game, the Packers decisively beat the Rams. Then the Bills beat the Ravens. The Cinderella Browns turned into pumpkins as they were defeated by the Chiefs! In the final game the Tom Brady Buccaneers beat the Saints for the first time this season. 
+In the first game, the Tom Brady Buccaneers beat the Packers in Green Bay. It was a great contest coming down to the last seconds. 
 </p>
 
 <p>
-	So now the table is set. The Packers and Buccaneers will play for the NFC title. While the Bills and the Chiefs will play for the AFC title. Whoever survives those two matches will play for overall dominance in the Super Bowl!
+In the second game, the Chiefs beat the Bills in Kansas City. The Bills never got started as the Chiefs offense and defense dominated.
+</p>
+
+<p>
+So now the final table is set. The Super Bowl combatants will be the Tom Brady Buccaneers representing the NFC against the Kansas City Chiefs representing the AFC. This is the second straight Super Bowl for the Chiefs. And this will be the first time any team has both hosted and played in Super Bowl.
 </p>
 
 <p>
@@ -14,17 +18,12 @@ In the first game, the Packers decisively beat the Rams. Then the Bills beat the
 </p>
 
 <p>
-This officially ends the Divisionals. On to Conference Supremacy Weekend!
+This officially ends the Conference finals. On to the Super Bowl!
 </p>
 
 <p>
-Games are continuing to diminish and the winning picks are becoming more elusive. So picking becomes more critical. Good look to the few that still have a chance to reign supreme at the end of the Season! 
+This is it. The cream has risen to the top. Two will go in and only one will come out! Good luck to the few that still have a chance to reign supreme at the end of the Season! 
 </p>
-
-<p>
-We add playoff games as soon as the date and time become available!
-</p>
-
 
 <p>
 The full stats are below! Read em and weep (I know I do)!
