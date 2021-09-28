2cfa3d01fcd98c067abe0b5ea5c9cb3f45efd944
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-09-28 19:59:09 -0400

    week 4 with statistics

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -18,6 +18,10 @@ In the last game of the week, the Cowboys got their second win by bludgeoning th
 </p>
 
 <p>
+  <span style='font-weight:bold; color:#FF6E3B;'>All in all a great week of football!</span>
+</p>
+
+<p>
 The full stats are below! Read em and weep (I know I do)!
 </p> 
 ";
