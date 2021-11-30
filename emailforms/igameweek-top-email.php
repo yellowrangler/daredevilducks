b587fc55fdd734c13eb3efffdabc2ca140a9ed9f
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-11-30 13:09:15 -0500

    season 2021 week 12 nostats

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,7 @@
 
 $emailtopwriteup = "	
 <p>
-The week got started with the Patriots crushing the Falcons.
-</p>
-
-<p>
-The Eagles clawed the Saints in a blowout. The Dolphins beat the Jets in a snore. Washington beat the Panthers. The Colts stampeded the Bills. The Browns eecked by the Lions. The 49ers crushed the Jaguars. In a huge upset, the Texans roped and branded the Titans. The Vikings beat the Packers.
-</p>
-
-<p>
-The Ravens beat the Bears. The Bengals clawed and beat the Raiders. The Cardinals beat the Seahawks. The Chiefs scalped the Cowboys. In the final Sunday game, the Chargers electrocuted the Steelers in a back and forth affair.
-</p>
-
-<p>
-In the last game of the week, the Buccaneers boarded and savaged the Giants!  
+Teams won and Teams Lost. That's the short version.
 </p>
 
 <p>
