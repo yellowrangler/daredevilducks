f190337b96fb474a6ddfe0e987f9cec249f394e1
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-12-08 13:37:29 -0500

    season 2021 week 13 stats

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,7 +2,20 @@
 
 $emailtopwriteup = "	
 <p>
-Teams won and Teams Lost. That's the short version.
+The week got started with the Cowboys beating the Saints.
+</p>
+
+<p>
+The Dolphins crushed the Giants. The Colts whopped on the Texans. The Lions took their first complete win from the Vikings.
+The Eagles beat the Jets. The Cardinals beat the Bears. The Chargers beat the Bengals. The Buccaneers beat the Falcons, to stop their loosing skid. The Rams crushed the Jaguars. The Redskins beat the Raiders. The Steelers beat the Ravens.
+</p>
+
+<p>
+The Seahawks beat the 49ers. The Chiefs beat the Broncos.
+</p>
+
+<p>
+In the last game of the week, the Patriots beat the Bills in Buffalo!
 </p>
 
 <p>
