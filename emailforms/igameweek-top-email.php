dc8bd2bc3db8f059ca2a432bcc46d260ecc4f2f2
Tarrant Cutler <tarrant.cutler@gmail.com>
2021-12-22 15:52:57 -0500

    season 2021 week 15 stats yes

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,23 @@
 
 $emailtopwriteup = "	
 <p>
-The week got started with the Vikings beating the Steelers.
+The week got started with the Chiefs beating the Chargers.
 </p>
 
 <p>
-The Cowboys held on to beat the Redskins. The Titans shut out the Jaguars. The Seahawks stayed viable by dispatching the Texans. The Chiefs crushed the Raiders. The Saints embarrassed the Jets in New York. The Falcons eeked by the Panthers. 
+On Saturday, the Colts beat the Patriots in what was a big game for both teams. The second Saturday game was moved to Monday due to Covid issues.
 </p>
 
 <p>
-The Browns beat the Ravens. The Chargers beat the woeful Giants. The Broncos crushed the cowardly Lions. The 49ers held on to beat the mercurial Bengals. The Bills lost in a heart breaker to the Tom Brady Buccaneers. The Packers beat the Bears.
+On Sunday, the Cowboys beat the Giants. The Texans beat the Jaguars (honestly who doesn't beat the Jaguars). The Steelers beat the Titans in a bit of an upset. The Steelers refuse to go away gracefully. Good for them. The Dolphins beat the Jets. The Lions beat the (OMG whatever has happened to the) Cardinals. The Bills beat the Panthers. The Bengals beat the Broncos. The 49ers beat the fading Falcons. In a close contest, the Packers held on to beat the Baltimore Ravens who tried to win the game with a last second 2 point conversion. The Saints shut out the Buccaneers in a very strange game.
 </p>
 
 <p>
-In the last game of the week, the Rams upset the Cardinals in Arizona!
+On Monday, the Raiders beat the Mayfield-less Browns. This was an especially tough loss for the Browns as they had a decent shot at making the playoffs but were bludgeoned by Covid and the NFL's seemingly arbitrary policies of what games to move when. The Vikings beat the Bears.
+</p>
+
+<p>
+On Tuesday, the Rams beat the Seahawks. The Eagles beat the Redskins.
 </p>
 
 <p>
