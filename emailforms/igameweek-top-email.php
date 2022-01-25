04f93fdfe9541049c5d5daa58c14c12020fe1042
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-01-24 19:05:00 -0500

    season 2021 week 20 nostats nobracket

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,19 @@
 
 $emailtopwriteup = "	
 <p>
-On Saturday, the Bengals beat the Raider in a close contest. The Bills crushed and embarrassed the Patriots.
+On Saturday, the Bengals beat the Titans in a close contest that was won by a last second field goal. The 49ers beat the number 1 seeded Packers, and again by a last second field goal.
 </p>
 
 <p>
-On Sunday, the Buccaneers beat the Eagles. The 49ers beat the Cowboys in a game where the Cowboys basically shot themselves in the head all game long with penalties. The Steelers miracle season ended with a loss to the Chiefs.
+On Sunday, the Rams beat the Buccaneers, again with a last second field goal! The Chiefs beat the Bills in a game that went into overtime, with the Chiefs winning the coin toss and ultimately the game.
 </p>
 
-<p>
-On Monday, the Rams dominated the Cardinals in what became a disappointing season for the Cardinals after a brilliant start.
-</p>
-
-<p>
+<!-- <p>
   <span style='font-weight:bold; color:#FF6E3B;'>All in all a great week of football!</span>
+</p> -->
+
+<p>
+  <span style='font-weight:bold; color:#FF6E3B;'>All in all a great Divisional week of football!</span>
 </p>
 
 <p style='padding-top:15px;'>
