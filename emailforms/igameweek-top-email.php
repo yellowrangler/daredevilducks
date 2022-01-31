64f7cb1f1170157a7be810c638eb769d9455eed4
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-01-31 18:41:42 -0500

    season 2021 week 21-22 nostats nobracket

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,15 @@
 
 $emailtopwriteup = "	
 <p>
-On Saturday, the Bengals beat the Titans in a close contest that was won by a last second field goal. The 49ers beat the number 1 seeded Packers, and again by a last second field goal.
+On Sunday, the Bengals stunned the football pedigree by beating the heavily favored Kansas City Chiefs. It was a game of two halves with the Chiefs taking a 18 point lead in the first half. In the second half the Bengals came back and tied the game, sending it into overtime. The Chiefs won the coin toss but Mahomes was intercepted. The Bengals never looked back, getting close enough to make the game winning field goal; catapulting them into the Super Bowl!
 </p>
 
 <p>
-On Sunday, the Rams beat the Buccaneers, again with a last second field goal! The Chiefs beat the Bills in a game that went into overtime, with the Chiefs winning the coin toss and ultimately the game.
+In the second game, the Rams beat the 49ers in a close contest. The Rams took the final lead late in the 4th quarter leaving the the 49ers under 2 minutes on the clock. Garoppolo was unable to move the team. In fact he threw an interception that confirmed that the Rams would be in the Super Bowl!
 </p>
 
-<!-- <p>
-  <span style='font-weight:bold; color:#FF6E3B;'>All in all a great week of football!</span>
-</p> -->
-
 <p>
-  <span style='font-weight:bold; color:#FF6E3B;'>All in all a great Divisional week of football!</span>
+  <span style='font-weight:bold; color:#FF6E3B;'>All in all a great week of Conference football!</span>
 </p>
 
 <p style='padding-top:15px;'>
