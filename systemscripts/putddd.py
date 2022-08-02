de4c3add855e6481869447dde79d85e4f20bc22c
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-08-02 08:44:46 -0400

    poop5

---
+++ systemscripts/putddd.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+#!/usr/bin/env python3
+
+import sys
+import subprocess
+import glob
+
+# global variables yea
+
+mysqlcommand = "mysql -u tarryc -p ddd < "
+
+file_list = []
+cmd = "no file selected"
+
+file_list = glob.glob('ddd-*python.sql')
+lcount = len(file_list)
+idx = 0
+while (idx < lcount):
+  print ('The count is:', lcount)
+  fname = file_list[idx]
+  print (fname)
+  answer = input('Do you want to use this file to update ddd database (yes/no) ?') 
+  Fl = answer.lower() 
+  if Fl == 'yes':  
+    cmd = mysqlcommand + fname
+    break;  
+  
+  idx += 1
+
+print ("The following command will now be run, ok? (yes/no)")
+print (cmd)
+
+answer = input('Do you want to use this file to update ddd database (yes/no) ?') 
+Fl = answer[0].lower() 
+if Fl == 'y':  
+	returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+	print('returned value:', returned_value)
+
+quit()
