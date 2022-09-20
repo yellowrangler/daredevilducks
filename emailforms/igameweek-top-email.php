f22c3b23cf3e7b65b42c0697576fc005581319a8
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-09-20 19:14:20 -0400

    season 2022 week 2 news and stats

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,19 @@
 
 $emailtopwriteup = "	
 <p>
-  It was a good opening week of football. We had close games, upsets and field goal gremlins! 
+  It was a good second week of football. We had the usual close games and upsets! 
 </p>
 
 <p>
-The Bills opened the season with an impressive win over last years Super Bowl winners Rams.  The Saints eked a win out over the Falcons. The Browns with Brissett beat the Panthers with Mayfield who used to quarterback the Browns. The Bears in a bit of an upset, beat the 49ers in a monsoon. In the most incredible game of the week, the Steelers beat the Bengals in a game featuring chances to win by both sides spoiled those field goal gremlins!
+The Chargers and Chiefs opened the week in a back and fourth affair with the Chiefs eeking out the win. The Dolphins improved their record to 2 and 0 by beating the Ravens. The Jets in an upset beat the Browns in Cleveland. The Lions beat the Commanders. The Jaguars shut out the Colts in a stunner, as the Colts with Ryan were expected to have a championship year. The Buccaneers beat their long time nemesis Saints in New Orleans, which featured as much fighting as football!
 </p> 
 
 <p>
-The Eagles beat the Lions. The Colts and Texans gave us our first tie of the season. The Dolphins crushed the Patriots. The Ravens beat the still hapless Jets evermore. The Commanders held on to beat the Jaguars. In another upset of sorts, the Giants beat the favored Titans in Nashville! The Chiefs pounded the Cardinals in Phoenix. The Chargers held on to beat the Raiders.
+The Giants gave New York fans hope as they went to 2 and 0 with a win over the Baker Mayfield led Jaguars. The Patriots won a tight back and forth contest over the Steelers in Pittsburgh. The Rams got back on track with a win over the Falcons. The 49ers behind Garoppolo thrashed the Seahawks. The Cowboys without Dak beat the winless Bengals. The Broncos got their first win of the season over the Texans.
 </p> 
 
 <p>
-The Vikings torched the Packers. The Buccaneers looked impressive in there win over the Cowboys in Dallas. In the final game for week 1, the Seahawks beat the Broncos in a thriller with a missed last second long distance field goal being the difference.
+The Cardinals came back in spectacular fashion to beat the winless Raiders. The Packers crushed the Bears in Green Bay. The Bills again looked sharp as they dismantled the Titans in Nashville. And in the final game of week 2 the Eagles beat the Vikings!
 </p> 
 
 <p>
