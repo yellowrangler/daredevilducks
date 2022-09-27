56a2320ae7b6ebdc5cccc16988310f227d3dfce2
Tarrant Cutler <tarrant.cutler@gmail.com>
2022-09-27 13:24:36 -0400

    season 2022  week 3. Add columbus day pic and stats.

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,24 +2,28 @@
 
 $emailtopwriteup = "	
 <p>
-  It was a good second week of football. We had the usual close games and upsets! 
+  It was a good third week of football. 
 </p>
 
 <p>
-The Chargers and Chiefs opened the week in a back and fourth affair with the Chiefs eeking out the win. The Dolphins improved their record to 2 and 0 by beating the Ravens. The Jets in an upset beat the Browns in Cleveland. The Lions beat the Commanders. The Jaguars shut out the Colts in a stunner, as the Colts with Ryan were expected to have a championship year. The Buccaneers beat their long time nemesis Saints in New Orleans, which featured as much fighting as football!
+The Browns and Steelers opened the week in an important divisional matchup that the Browns won. The Panthers got their first win defeating the Saints. The Bears won a close game over the Texans. The Colts got their first win over the highly touted Chiefs. The Dolphins handed the high flying Bills their first loss in an exciting back and forth affair. The Vikings narrowly pulled a victory over the much improved Lions.
 </p> 
 
 <p>
-The Giants gave New York fans hope as they went to 2 and 0 with a win over the Baker Mayfield led Jaguars. The Patriots won a tight back and forth contest over the Steelers in Pittsburgh. The Rams got back on track with a win over the Falcons. The 49ers behind Garoppolo thrashed the Seahawks. The Cowboys without Dak beat the winless Bengals. The Broncos got their first win of the season over the Texans.
+The Ravens beat the Patriots in Foxborough. The Bengals got their first win against the Jets. The Titans got their first win against the winless Raiders. The Eagles remained undefeated as they mauled the Commanders. In an upset of sorts, the Jaguars beat the Chargers. Atlanta beat the Seahawks. Green Bay gave the Buccaneers their first loss in Tampa Bay. The Broncos held on to defeat the 49ers.
 </p> 
 
 <p>
-The Cardinals came back in spectacular fashion to beat the winless Raiders. The Packers crushed the Bears in Green Bay. The Bills again looked sharp as they dismantled the Titans in Nashville. And in the final game of week 2 the Eagles beat the Vikings!
+In the final game of week 3 the Cowboys defeated the Giants in New York!
 </p> 
 
 <p>
   <span style='font-weight:bold; color:#4682B4;'>All in all a superb week of football!</span>
 </p> 
+
+<p>
+Don't be disappointed if you are not basking in the upper wins tier! It is still very early in the season. Just keep sharpening your pencil and encourage those little grey cells to do their stuff! You can do it!
+</p>
 
 <br />
 <p>
