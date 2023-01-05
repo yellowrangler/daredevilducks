9966b71909835b154eaa5057a17111de1a9eebf6
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-01-05 15:00:45 -0500

    season-2022 week-17 stats final

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,15 @@
 
 $emailtopwriteup = "	
 <p>
-The week started with the Jaguars beating the Jets. The Panthers beat the Lions. The Bills beat the Bears in freezing temperatures. The Saints came marching in as they beat the Browns. The Chiefs scalped the Seahawks and clinched their division. The Vikings clinched their division by edging the Giants. 
+The week started with the Cowboys beating the fading Titans. The Falcons beat the Cardinals. The Lions kept their playoff hopes alive by beating the Bears. The Jaguars crushed the Texans. The Chiefs squeaked by the Broncos. The Patriots beat the Dolphins. The Giants put a whooping on the Colts. The Saints beat the Eagles.
 </p> 
 
 <p>
- The Bengals beat the hapless Patriots. The Texans upset the Titans. The Ravens beat the Falcons and Shall be lifted-nevermore! The 49ers crushed the Commanders. The Cowboys beat the Minshew led Eagles. The Steelers stayed on the playoff bubble by disposing of the Raiders. In a bit of an upset, the Packers beat the Dolphins. The Rams bludgeoned the Broncos. Tom Brady pulled a Tom Brady leading the Buccaneers to a victory over the Cardinals in overtime.
+ The Buccaneers came from behind to beat the Panthers. The Browns beat the Commanders. The 49ers came from behind to beat the Raiders. The Seahawks beat the struggling Jets. The Chargers beat their sibling Rams. Green Bay stayed relevant as they dispatched the Vikings. The Steelers came from behind to beat the Ravens.
 </p> 
 
 <p>
-In the final game of the week the Chargers beat the Colts to guarantee themselves a seat at the playoff table!. 
-</p> 
-
-<p>
-  All in all a superb week of football!
+In the final game of the week tragedy struck as Buffalo's Damar Hamlin suffered a cardiac arrest on the field. The game was postponed and at this time has not been rescheduled. According to the NFL website the Bills have announced that Damar Hamlin has shown 'remarkable improvement' and 'appears to be neurologically intact'. Lets all pray that he makes a full recovery.
 </p> 
 
 <p>
