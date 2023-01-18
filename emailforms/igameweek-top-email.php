9524e8bbe3d4e1d650e887c970ba9c2f58e0f36d
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-01-18 08:34:09 -0500

    season-2022 week-20-start-stats-bracket

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,24 +2,16 @@
 
 $emailtopwriteup = "	
 <p>
-The week started with the Chiefs punishing the Raiders. The Jaguars knocked the Titans out of the playoffs and inserted themselves instead! The Falcons beat the Buccaneers. The Bills knocked the Patriots out of the playoffs. The Vikings crushed the Bears. The Bengals beat the Ravens. The Texans eeked a win over the Colts.
+The week started with the 49ers slapping the Seahawks. The Jaguars made a statement as they beat the Chargers. The Bills held on to dispatch the Dolphins. The Giants upset the Vikings at home. The Bengals beat the Ravens with a crazy fumble run for a touchdown.
 </p> 
 
 <p>
- The Dolphins beat the Jets in a snoozer. The Panthers beat the Saints. The Steelers beat their division rival Browns. The Broncos beat the Chargers. The Eagles beat the Giants. The Seahawks beat the Rams earning them a place in  the playoffs and simultaneously knocking the Steelers out. The 49ers crushed the Cardinals and the Commanders beat the Cowboys.
+In the final game, the Cowboys crushed the Buccaneers as the Tom Brady Cowboy dominance came to an ugly end!
 </p> 
 
 <p>
-In the final game of the week the Lions beat the Packers knocking them out of the playoffs.
+  All in all a superb week of Playoff football!
 </p> 
-
-<p>
-  All in all a superb week of football!
-</p> 
-
-<p>
-The regular season is over!  Welcome to Wildcard Weekend!
-</p>
 
 <p>
 Check out our Teams Playoff Bracket, accessible by clicking on the large Daredevil Ducks image on the main page. Mobile users swipe your Avatar image to the left until you see the Teams Playoff Bracket. Then select the image. Check it out!
