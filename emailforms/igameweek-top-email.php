43c67674fe81ca7ecacea4285eb73678bf41abf9
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-01-27 15:24:50 -0500

    season2022-week-21-start-stats

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,16 +2,20 @@
 
 $emailtopwriteup = "	
 <p>
-The week started with the 49ers slapping the Seahawks. The Jaguars made a statement as they beat the Chargers. The Bills held on to dispatch the Dolphins. The Giants upset the Vikings at home. The Bengals beat the Ravens with a crazy fumble run for a touchdown.
+The week started with the Chiefs beating the Jaguars in a closer then expected match.  The Eagles crushed the Giants. The Bengals handsomely beat the Bills with snowy conditions in Buffalo!
 </p> 
 
 <p>
-In the final game, the Cowboys crushed the Buccaneers as the Tom Brady Cowboy dominance came to an ugly end!
+In the final game, the 49ers eked out a win over the Cowboys!
 </p> 
 
 <p>
   All in all a superb week of Playoff football!
 </p> 
+
+<p>
+Only 3 games are left! The winning window is almost closed! Pick wisely!
+</p>
 
 <p>
 Check out our Teams Playoff Bracket, accessible by clicking on the large Daredevil Ducks image on the main page. Mobile users swipe your Avatar image to the left until you see the Teams Playoff Bracket. Then select the image. Check it out!
@@ -21,4 +25,5 @@ Check out our Teams Playoff Bracket, accessible by clicking on the large Daredev
 The full stats are below! Read em and weep (I know I do)!
 </p> 
 ";
+
 ?>
