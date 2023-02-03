d4d1d9045e59144b8868f9beb4be13da979720eb
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-02-02 21:23:22 -0500

    season2022-week22-superbowl-stats-brackets-start

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,23 +2,24 @@
 
 $emailtopwriteup = "	
 <p>
-The week started with the Chiefs beating the Jaguars in a closer then expected match.  The Eagles crushed the Giants. The Bengals handsomely beat the Bills with snowy conditions in Buffalo!
+In the NFC conference playoffs the Eagles beat the 49ers. The 49ers lost both their second and third string quarter backs early in the game. I have never seen such a thing happen. It was a shame as it reduced the game to a fete de complete for the Eagles!
 </p> 
 
 <p>
-In the final game, the 49ers eked out a win over the Cowboys!
+In the AFC conference playoffs the Chiefs beat the Bengals. It was a competitive match up. Unfortunately for Cincinnati penalties where devastating. Especially the last penalty which put the Chiefs in range for a winning field goal!
 </p> 
 
+<
 <p>
   All in all a superb week of Playoff football!
 </p> 
 
 <p>
-Only 3 games are left! The winning window is almost closed! Pick wisely!
+Only the Super Bowl is left! The winning window is down to its last gasp! Pick wisely! Praying probably wouldn't hurt either!
 </p>
 
 <p>
-Check out our Teams Playoff Bracket, accessible by clicking on the large Daredevil Ducks image on the main page. Mobile users swipe your Avatar image to the left until you see the Teams Playoff Bracket. Then select the image. Check it out!
+Check out our Teams Playoff Bracket, accessible by clicking on the large Daredevil Ducks image on the main page. Mobile users swipe your Avatar image to the left until you see the Teams Playoff Bracket. Then select the image. Check it out! You may need to refresh your browser to bring in the new bracket image.
 </p>
 
 <p>
