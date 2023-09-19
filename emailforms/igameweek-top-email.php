b600b3b6125bd4c812928ec06b0d32b80a27bbe3
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-09-19 14:34:13 -0400

    end week2 nostats dialog

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,15 @@
 
 $emailtopwriteup = "	
 <p>
-The first game pitched the defending 2022 Super Bowl Champs Chiefs against the upstart Lions in Kansas. In a thriller the Lions beat the Chiefs by 1 point! The Falcons looked strong as they dispatched the Panthers who rolled out their highly touted draft pick Bryce Young. The Browns crushed the Bengals. The Jaguars with Trevor Lawrence beat the Colts. The Buccaneers without Tom Brady beat the Vikings! The Saints with Derek Car beat the disappointing Titans. The 49ers crushed the Steelers in Pittsburgh. The Ravens corralled the Texans. The Commanders beat the Cardinals.
+In the first game found the Eagles won over the Vikings. The Falcons looked strong again in their win over the Packers. The Bills got their first win of the season at the expense of the Raiders. Baltimore beat their division rivals Bengals in a close game. The Seahawks beat the Lions and the Colts beat the Texans.
 </p> 
 
 <p>
-The Rogers-less Packers beat the Bears. The Raiders with their new quarterback Jimmy Garoppolo held on to beat the Broncos. The Eagles eeked a win over the Patriots in Foxborough. The Dolphins won in a squeaker over the Chargers. The Rams played well as they dispatched an equally good Seahawks team. The Cowboys absolutely stampeded the pathetic Giants.
+In a low scoring game, the Chiefs beat the Jaguars. The Titans won in a squeaker against the Chargers. The Buccaneers continued to win as they dispatched the Bears. The Giants got their first twin as they eeked by the winless Cardinals. In a California rumble, the 49ers outlasted the Rams. The Cowboys crushed the still Roger-less Jets. The Commanders kept the Broncos and Sean Payton winless. The Dolphins beat the winless Patriots.
 </p>  
 
 <p>
-In the final game, the Jets beat the highly touted Bills in a game that saw the Jets new all star quarterback, Rodgers, injured on the first series of downs. Mistakes by the Bills Allen and the strength of the Jets defense defined the story!
-</p>  
-
-<p>
-  All in all a superb start to the season!
+  All in all a superb week of football!
 </p>  
 
 <p>
