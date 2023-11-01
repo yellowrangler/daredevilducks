c4ca585abc43ffbeeeca147342ad24e9c08dd53d
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-11-01 19:03:03 -0400

    season 2023 week 8 end dialog nostats injury

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,19 @@
 
 $emailtopwriteup = "	
 <p>
-In the first game the Jaguars beat the Saints in New Orleans. The Bears beat the Raiders. In a tight back and forth affair, the Browns held on to beat the Colts. In an upset, the Patriots came back to beat the Bills. The Giants beat the Commanders and the Ravens beat the Lions.
+In the first game the Bills  beat a competitive Buccaneers team. The Panthers eked out their first win over the Texans. The Saints beat the Colts in Indianapolis. The Cowboys beat the Rams. The Vikings beat the disappointing Packers. The Dolphins beat the Patriots. 
 </p> 
 
 <p>
-The Falcons eked out a win over the Buccaneers. The Steelers beat the Rams. The Seahawks beat the Cardinals. The Broncos got their second win defeating the Packers. The Chiefs crushed the Chargers. The Eagles clawed the Dolphins.
+In the battle for New York, the Jets beat their state rival Giants. The Titans behind Levis beat the Falcons. The Jaguars beat the Steelers. In a slug fest, the Eagles beat the Commanders. The Seahawks beat the Browns. The Ravens beat the Cardinals. In the upset of the week, the Broncos crushed the Chiefs. The Chargers beat the Bears.
 </p>
 
 <p>
-In the Monday night game, the Vikings beat the 49ers!
-</p>  
+In the Monday night game, the Lions beat the Raiders!
+</p> 
 
 <p>
-There is still a lot of time! Don't get Spooked!
+There is still a lot of time! You can do it!
 </p> 
 ";
 
