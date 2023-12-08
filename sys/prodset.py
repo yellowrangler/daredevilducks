27a2f3ff09c19c39574f3eec6a3972dae6812db6
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-12-08 17:22:14 -0500

    season2023-week14-injury-update

--- sys/prodset.py
+++ sys/prodset.py
@@ -9,16 +9,29 @@ import shutil
 from pathlib import Path
 from datetime import date
 
-#
-# global variables
-#
-token_desktop = "DDV"
-token_mobile = "DMV"
-token = ""
+######################################################################################################
+# Author: Tarrant Cutler Jr
+# Date: 12/07/2023
+# Description: Change ddd index file version numbers
+######################################################################################################
 
-#
+##################################
+# global variables
+##################################
+
+desktop_target = 'desktop'
+mobile_target = 'mobile'
+
+tokens = {
+    'desktop': 'DDV',
+    'mobile': 'DMV'
+}
+
+filename_paths = dict();
+
+##################################
 # functions
-#
+##################################
 
 #
 # check valididty of file name
@@ -27,95 +40,169 @@ def check_filename(path_to_file):
     path = Path(path_to_file)
 
     if path.is_file():
-        print(f'The file {path_to_file} exists')
+        # print(f'The file {path_to_file} exists')
         return 1
     else:
         print(f'The file {path_to_file} does not exist')
         return 0
  
 #
-# get old version id from bakup
+# get old version id 
 #
-def get_ver_id(source): 
-    with open(source,'r') as file:
-        fileStr = file.read()
-        token_offset = fileStr.index(token)
-        olddate = fileStr[token_offset+3:token_offset+11]
-        # print(f'oldversion = {oldversion}')
+def get_ver_id(token, source): 
+  with open(source,'r') as file:
+      fileStr = file.read()
+      token_offset = fileStr.index(token)
+      olddate = fileStr[token_offset+3:token_offset+11]
+      # print(f'oldversion = {oldversion}')
 
-    #return old version
-    oldversion = token + olddate
-    return oldversion 
+  oldversion = token + olddate
+  return oldversion 
+
+#
+# set global file paths
+#
+def set_file_paths(): 
+    username = os.getlogin()
+
+    d = dict();
+    d['desktop'] = f'/home/{username}/Development/daredevilducks/index.html'
+    d['mobile'] = f'/home/{username}/Development/daredevilducks/mobile/index.html'
+    
+    return d  
+
+#
+# get old and new version id
+#
+def get_old_new_version(token, fp):
+  v = dict()
+
+  # get old version
+  oldver = get_ver_id(token, fp)
+
+  # set new version
+  newdate = date.today().strftime('%m%d%Y')
+  newver = token + newdate   
+
+  v['oldver'] = oldver
+  v['newver'] = newver      
+
+  return v
  
+# 
+#  get target input parm
+#  
+def get_target_input():
+  target = input("Enter target product (desktop, mobile or both): ")
 
-#
-# get the usernae from the os. we need it to build file name
-#
-username = os.getlogin()
-filename_desktop = f'/home/{username}/Development/daredevilducks/index.html'
-filename_mobile = f'/home/{username}/Development/daredevilducks/mobile/index.html'
-
-#
-# set the filename from product target
-#
-target = input("Enter target product (desktop or mobile: ")
-
-if (len(target) == 0):
-    print("Must pass in valid target product! desktop or mobile")
+  if (len(target) == 0):
+      target = 'both'
+  elif (target != 'desktop' and target != 'mobile' and target != 'both'):
+    print("Must pass in valid target product! desktop, mobile or both")
     quit()
 
-if (target == 'desktop'):
-    filename = filename_desktop 
-    token = token_desktop
-    # print("target product is desktop")
-elif (target == 'mobile'):
-    filename = filename_mobile 
-    token = token_mobile
-    # print("target product is mobile")
-else:
-    print("Must pass in valid target product! desktop or mobile")
+  return target
+
+#
+# process request
+#
+def process_target(target, versions, current_filepath):
+  rc = check_filename(current_filepath)
+  if (rc == 0):
+    quit()  
+
+  answer = input(f"\nApply new version update for {target}: (y/n) ")
+  if (answer[0] == 'y' or answer[0] == 'Y'):
+    print(f'Applying {target} Update!')
+  else:
     quit()
 
+  # print out parms
+  print(f'target = {target}')
+  print(f'filename = {current_filepath}')
+  print(f'oldver = {versions["oldver"]}')
+  print(f'newver = {versions["newver"]}')  
+
+  #
+  # apply new version to file
+  #
+  with fileinput.FileInput(current_filepath, inplace=True, backup='.bak') as file:
+      for line in file:
+          print(line.replace(versions["oldver"], versions["newver"]), end='')  
+
+  print(f'Target = {target} updated')
+
 #
-# check if filename exists
+# process request
 #
-rc = check_filename(filename)
-if (rc == 0):
+def process_request(target, fps):
+
+  if target == desktop_target:
+    #
+    # desktop only
+    #
+    current_token = tokens[desktop_target]
+    current_filepath = fps[desktop_target]
+
+    versions = get_old_new_version(current_token, current_filepath)
+
+    process_target(target, versions, current_filepath)
+  elif target == mobile_target:
+    #
+    # mobile only
+    #
+    current_token = tokens[mobile_target]
+    current_filepath = fps[mobile_target]
+
+    versions = get_old_new_version(current_token, current_filepath)
+
+    process_target(target, versions, current_filepath)
+  elif target == 'both':
+    #
+    # desktop first
+    #
+    current_token = tokens[desktop_target]
+    current_filepath = fps[desktop_target]
+
+    versions = get_old_new_version(current_token, current_filepath)
+
+    process_target(desktop_target, versions, current_filepath)
+
+    #
+    # mobile second
+    #
+    current_token = tokens[mobile_target]
+    current_filepath = fps[mobile_target]
+
+    versions = get_old_new_version(current_token, current_filepath)
+
+    process_target(mobile_target, versions, current_filepath)
+  else:
+    print(f'Invalid target {target}')
     quit()
 
-#
-# get old version
-#
-oldver = get_ver_id(filename)
+
+##################################
+# Main
+##################################  
+
+print(f'Version update started!\n')
 
 #
-# set new version
+# get the username from the os. And set the file path to target index files
 #
-newdate = date.today().strftime('%m%d%Y')
-newver = token + newdate
-# print(f'newver = {newver}')
-
-# print('\n')
-print(f'target = {target}')
-print(f'filename = {filename}')
-print(f'oldver = {oldver}')
-print(f'newver = {newver}')
-
-answer = input("Apply new version update: (y/n) ")
-if (answer[0] == 'y' or answer[0] == 'Y'):
-    print('Applying Update!')
-else:
-    quit()
+filename_paths = set_file_paths()
 
 #
-# apply new version to file
+# set the gloal target choice
 #
-with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
-    for line in file:
-        print(line.replace(oldver, newver), end='')
+target = get_target_input()
 
-print(f'filename = {filename} updated')
+#
+# process based on target
+#
+process_request(target, filename_paths)
+
+print(f'\nVersion update finished!')
 
 quit()
-
-
--- sys/prodset2.py
+++ sys/prodset2.py
@@ -9,32 +9,16 @@ import shutil
 from pathlib import Path
 from datetime import date
 
-######################################################################################################
-# Author: Tarrant Cutler Jr
-# Date: 11/04/2023
-# Description: Read in bank or creditcard csv information
-######################################################################################################
-
-##################################
+#
 # global variables
-##################################
+#
+token_desktop = "DDV"
+token_mobile = "DMV"
+token = ""
 
-tokens = {
-    'desktop': 'DDV',
-    'mobile': 'DMV'
-}
-
-filename_paths = dict();
-targets = dict();
-
-versions = {
-  'oldver': '',
-  'newver': ''
-}
-
-##################################
+#
 # functions
-##################################
+#
 
 #
 # check valididty of file name
@@ -50,153 +34,37 @@ def check_filename(path_to_file):
         return 0
  
 #
-# get old version id 
+# get old version id from bakup
 #
-def get_ver_id(source, tid): 
-  current_token = token[tid]
-  with open(source,'r') as file:
-      fileStr = file.read()
-      token_offset = fileStr.index(current_token)
-      olddate = fileStr[token_offset+3:token_offset+11]
-      # print(f'oldversion = {oldversion}')
+def get_ver_id(source): 
+    with open(source,'r') as file:
+        fileStr = file.read()
+        token_offset = fileStr.index(token)
+        olddate = fileStr[token_offset+3:token_offset+11]
+        # print(f'oldversion = {oldversion}')
 
-  #return old version
-  oldversion = current_token + olddate
-  return oldversion 
-
-#
-# set global file paths
-#
-def set_file_paths(): 
-    username = os.getlogin()
-
-    d = dict();
-    d['desktop'] = f'/home/{username}/Development/daredevilducks/index.html'
-    d['mobile'] = f'/home/{username}/Development/daredevilducks/mobile/index.html'
-    
-    return d  
-
-#
-# get old and new version id
-#
-def get_old_new_version(fp):
-  # get old version
-  oldver = get_ver_id(fp)
-
-  # set new version
-  newdate = date.today().strftime('%m%d%Y')
-  newver = token + newdate   
-
-  versions['oldver'] = oldver
-  versions['newver'] = newvar      
+    #return old version
+    oldversion = token + olddate
+    return oldversion 
  
-# 
-#  get target input parm
-#  
-def get_target_input():
-  target = input("Enter target product (desktop or mobile: ")
 
-  if (len(target) == 0):
-      target = 'both'
-  elif (target != 'desktop' and target != 'mobile' and target != 'both'):
-    print("Must pass in valid target product! desktop, mobile or both")
+#
+# get the usernae from the os. we need it to build file name
+#
+username = os.getlogin()
+filename_desktop = f'/home/{username}/Development/daredevilducks/index.html'
+filename_mobile = f'/home/{username}/Development/daredevilducks/mobile/index.html'
+
+#
+# set the filename from product target
+#
+target = input("Enter target product (desktop or mobile: ")
+
+if (len(target) == 0):
+    print("Must pass in valid target product! desktop or mobile")
     quit()
 
-  return target;
-
-#
-# process request
-#
-process_target(t, fp):
-  filename = fp  
-
-  rc = check_filename(filename)
-  if (rc == 0):
-    quit()  
-
-  answer = input("Apply new version update for desktop: (y/n) ")
-  if (answer[0] == 'y' or answer[0] == 'Y'):
-    print('Applying Deaskto Update!')
-  else:
-    quit()
-
-  #
-  # apply new version to file
-  #
-  with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
-      for line in file:
-          print(line.replace(oldver, newver), end='')  
-
-#
-# process request
-#
-process_request(target, fp):
-
-  if target == 'desktop':
-    current_token = token['desktop']
-    current_filepath = fp['desktop']
-    
-    get_old_new_version(current_token, versions, current_filepath)
-
-    process_desktop(v, fp['desktop'])
-  elif target == 'mobile':
-    process_mobile(v, fp['mobile'])
-  else:
-    process_desktop(v, fp['desktop'])
-    process_mobile(v, fp['mobile'])  
-
-
-
-
-
-
-
-
-
-
-
-
-  filename = filename_desktop 
-  token = token_desktop  
-
-  rc = check_filename(filename)
-  if (rc == 0):
-    quit()
-
-
-
-##################################
-# Main
-##################################  
-
-#
-# get the username from the os. And set the file path to target index files
-#
-filename_paths = set_file_paths()
-print(filename_paths)
-
-#
-# set the filename target
-#
-target = get_target_input()
-
-#
-# process based on target
-#
-process_request(target, filename_paths)
-
-
-
-
-
-
-
-
-
-
-
-
-
+if (target == 'desktop'):
     filename = filename_desktop 
     token = token_desktop
     # print("target product is desktop")
@@ -249,5 +117,3 @@ with fileinput.FileInput(filename, inplace=True, backup='.bak') as file:
 print(f'filename = {filename} updated')
 
 quit()
-
-
