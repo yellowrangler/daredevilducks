e578d6468a9132cadaf8dc16534c06952ad8b09b
Tarrant Cutler <tarrant.cutler@gmail.com>
2023-12-27 15:05:24 -0500

    season-2023 week-17 start nostats no injury

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,21 +2,21 @@
 
 $emailtopwriteup = "	
 <p>
-In the first game the Raiders eviscerated the Chargers. The Bengals held on to beat the Vikings. The Colts beat the Steelers. The Lions mauled the Broncos. The Panthers beat the Falcons. The Browns beat the Bears in the wackiest game of the week. The Buccaneers beat the Packers in Green Bay.
+In the opening game for the week, the Rams beat the Saints. The Steelers crushed the Bengals. The Bills held the course by beating the Chargers in Los Angles. The Falcons beat the Colts. The Packers eked out a win over the Panthers. The Browns beat the Texans in Carolina.
 </p> 
 
 <p>
-The Dolphins shut out the Jets. The Saints crushed the Giants. The Texans held on to beat the Titans at home in overtime. The Chiefs crushed the Patriots. The 49ers beat the Cardinals in Arizona. The Rams played well enough to beat the Commanders in a close contest. The Bills ran the Cowboys out of town in an impressive beat down. The Ravens beat the Jaguars.
+The Lions won their Division title by beating the Vikings in Minnesota. The Jets held on to beat the Commanders in a back and forth match. The Seahawks held on to win over the Titans in a closely played contest in Tennessee. The Buccaneers beat the Jaguars. The Bears beat the Cardinals. In the game of the week, the Dolphins beat the Cowboys. In the upset of the week, the Patriots beat the Broncos in Denver.
 </p>
 
 <p>
-For Monday night, the Seahawks beat the highly touted Eagles. 
+For Monday night we had three games. In the first, the Raiders beat their nemeses the Chiefs in Kansas City. The Eagles beat the Giants. And in the final game, the Ravens manhandled the 49ers.
 </p>  
 
-<p>
-The Wheel of time is now crushing competition for the regular season! If your not near the top... Just do the math! For those still in it - Good Luck! Go for it! 
-</p>
 
+<p>
+The Wheel of time has crushed most of the competition for the regular season! If your not near the top... Just do the math! For those still in it - Good Luck! Go for it! 
+</p>
 ";
 
 ?>
