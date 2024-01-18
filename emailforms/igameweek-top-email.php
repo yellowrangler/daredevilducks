3f82d22f5b30953d907d2c213c5c7f3530e0c045
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-01-17 21:24:21 -0500

    season-22023 week-20 stats dialog injury

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,23 +2,23 @@
 
 $emailtopwriteup = "	
 <p>
-In the opening game for the week, the Steelers beat the Lamar-less Ravens. This was a pivotal game for the Steelers, as it propelled them into the Playoffs! The Texans beat the Colts, propelling them into the playoffs as well! The Buccaneers beat the Panthers, clinching the Division title for the Buccaneers! The Bengals beat the depleted Browns. The Lions beat the Vikings. The Saints beat the Falcons. The Jets beat the Patriots in New England. The first time for the Jets in New England in 15 years! 
+In the opening game, the Texans put an old style shellacking on the Browns.  Too many mistakes by Cleveland! In a return to the ice age (Climate Warming? Really?) the Chiefs beat the warm weather Dolphins. Just saying...
 </p> 
 
 <p>
-The Titans beat the Jaguars in a back and forth affair. The Seahawks beat the Cardinals. The Packers beat the Bears, earning them a place in the playoffs! The Giants beat the Eagles. The Raiders beat the Broncos. The Chiefs beat the Chargers. The Rams beat the 49ers. The Cowboys beat the Commanders.
+The Packers upset the Cowboys in Dallas! Jerry Jones looked like he was ready to skin someone. Belichick opportunity? The Lions beat the Rams in  nail biter! The old quarterback for the Rams, who was now the quarterback for the Lions, beat the former quarterback of the Lions who was now the quarterback for the Rams. Yea... Try saying that 5 times fast! At the snow bowl, the Bills beat a tough Steelers team. 
 </p>
 
 <p>
-In the final game of the week, the Bills beat the Dolphins.
+In the final game of the week, the Buccaneers crushed the disappointing Eagles. The Eagles are another team where changes will be coming!
 </p>  
 
 <p>
-Welcome to the post season! This should be a very entertaining week! Enjoy!
-</p>
+  <span style='color:#40B7E0;font-weight:900;'>All in all a superb week of football! On to Divisional Playoffs, week 20!</span>
+</p> 
 
 <p >
-	Check out  our Wild Card week <b>Team Bracket</b>! You can see it by clicking on the <b>Donald Duck on a Rocket</b> image. As the Playoffs progress, the <b>Team Bracket</b> will change as well! 
+	Check out our Divisional Playoffs <b>Team Bracket</b>! You can see it by clicking on the <b>Donald Duck on a Rocket</b> image. As the Playoffs progress, the <b>Team Bracket</b> will change as well! 
 </p>
 
 <p> 
