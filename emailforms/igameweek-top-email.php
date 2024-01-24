63da889786671f7fc9a9b15bc3e09544a5876f50
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-01-24 15:28:45 -0500

    season-2023 week-21 start dialog injurys stats version

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,23 +2,19 @@
 
 $emailtopwriteup = "	
 <p>
-In the opening game, the Texans put an old style shellacking on the Browns.  Too many mistakes by Cleveland! In a return to the ice age (Climate Warming? Really?) the Chiefs beat the warm weather Dolphins. Just saying...
+The Ravens beat the Texans decisively in the first Saturday Wildcard weekend game. In the second Saturday game the 49ers won over the Packers in a closer contest.
 </p> 
 
 <p>
-The Packers upset the Cowboys in Dallas! Jerry Jones looked like he was ready to skin someone. Belichick opportunity? The Lions beat the Rams in  nail biter! The old quarterback for the Rams, who was now the quarterback for the Lions, beat the former quarterback of the Lions who was now the quarterback for the Rams. Yea... Try saying that 5 times fast! At the snow bowl, the Bills beat a tough Steelers team. 
+In the first Sunday game, the Lions beat the Buccaneers in a competitive match. In the second and final Sunday game, the Chiefs beat the Bills in a heart breaker for the Bills. Seems like missing last second field goals is still in the Bills DNA.
 </p>
 
 <p>
-In the final game of the week, the Buccaneers crushed the disappointing Eagles. The Eagles are another team where changes will be coming!
-</p>  
-
-<p>
-  <span style='color:#40B7E0;font-weight:900;'>All in all a superb week of football! On to Divisional Playoffs, week 20!</span>
+  <span style='color:#40B7E0;font-weight:900;'>All in all a superb week of football! On to the Conference Playoffs, week 21!</span>
 </p> 
 
 <p >
-	Check out our Divisional Playoffs <b>Team Bracket</b>! You can see it by clicking on the <b>Donald Duck on a Rocket</b> image. As the Playoffs progress, the <b>Team Bracket</b> will change as well! 
+	Check out our Conference Playoffs <b>Team Bracket</b>! You can see it by clicking on the <b>Donald Duck on a Rocket</b> image. As the Playoffs progress, the <b>Team Bracket</b> will change as well! 
 </p>
 
 <p> 
