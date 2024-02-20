8ab8486b90949684baf556796b626e198d13a661
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-02-20 11:37:38 -0500

    season-2023 week-22 superbowl final email add

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,24 +2,20 @@
 
 $emailtopwriteup = "	
 <p>
-The Chiefs beat the Ravens in the first Conference playoff game. The Ravens looked out of sorts from the start. They played punishing football but didn't come together until it was too late. Meanwhile, the Chiefs looked very good in all facets of the game. This may be a great chance for them to win a back to back Super Bowl.
-</p> 
-
-<p>
-In the second Conference playoff game The 49ers beat the Lions. The 49ers were good enough to beat the Lions. The Lions coaching staff made a couple of head scratching decisions regarding going for 4th and short when points were there as a field goal.
+The Chiefs beat the 49ers, and in so doing, secured the Lombardi Trophy for 2023! It wasn't a Super Bowl for the ages, but it delivered enough to keep most people watching to the end. If for no other reason then to see who won! Congratulations Chiefs in this your first repeat win.
 </p>
 
 <p>
-  <span style='color:#40B7E0;font-weight:900;'>All in all a superb week of football! On to the Super Bowl!</span>
+We had a great turnout this season! Thank you all again for participating. <span style='color:#FF6E3B;font-weight:700;'>Sville 5</span> is now our official <span style='font-weight:bold; color:maroon;'> Daredevil Ducks first place Champion</span> for the 2023 Season!  
+</p>
+
+<p>
+Please raise a glass of your preferred beverage to them. They owned first place for most of the last 3rd of the season! Not an easy thing to do. Congratulations team <span style='color:#FF6E3B;font-weight:700;'>Sville 5</span>.
+</p>
+
+<p>
+  <span style='color:#40B7E0;font-weight:900;'>All in all a superb Season of football! Hope to see you all next season!</span>
 </p> 
-
-<p >
-	Check out our Super Bowl Playoffs <b>Team Bracket</b>! You can see it by clicking on the <b>Donald Duck on a Rocket</b> image. As the Playoffs progress, the <b>Team Bracket</b> will change as well! 
-</p>
-
-<p> 
-	Mobile users swipe the icon at the bottom of the screen to the left until you see the <b>Team Bracket</b> image! Select that image for a larger picture! 
-</p>
 
 ";
 
