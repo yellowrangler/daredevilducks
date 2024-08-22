db6766267f755f2640d89a7fa187364ddc1d93a3
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-08-22 12:35:27 -0400

    teamweeklyranking fix

--- app/controllers/teamsController.js
+++ app/controllers/teamsController.js
@@ -1239,7 +1239,7 @@ controllers.teamweeklyrankingController = function ($scope, $http, $location, te
 
         var windowHeight = window.innerHeight;
         var windowWidth = window.innerWidth;
-        $scope.imageWidth = windowWidth * .15;  // was .15
+        $scope.imageWidth = windowWidth * .13;  // was .15
         $scope.current.WeeklRankingtablesize = windowHeight * .50;  // was .60
 
         $scope.$parent.tracker('team scores and stats',
