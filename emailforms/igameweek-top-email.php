6f15f775adb58e2a9002ebc869ee02997864fece
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-10-22 20:44:47 -0400

    chart man new

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -17,6 +17,10 @@ In the Monday night games, The Ravens beat the Buccaneers in Tampa. and the Card
 A great Seventh week! Don't be disappointed if you did not do well. Bee the ball!
 </p>
 
+<p>
+	We have introduced a new member <span style='color:#FF6E3B;font-weight:900;'>ChartMan</span>! ChartMan only picks the winners by looking at the <span style='color:#FF6E3B;font-weight:900;'>Teams Stats</span> popup on the <span style='color:#FF6E3B;font-weight:900;'>Pick your Teams</span> page! It will be interesting to see how he does against those of us (me included) who think through our own picks. Kind of like putting money in stock market indexes verses active investing! 
+</p>
+
 
 <p>
 The Member stats are below! Read em and weep (I know I do)!
