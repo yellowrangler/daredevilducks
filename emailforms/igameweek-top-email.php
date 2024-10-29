4eb6a17ffef127654819f90f8c072066d00ae173
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-10-29 09:07:05 -0400

    start season2024 week9 nostats noinjury

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,25 +2,20 @@
 
 $emailtopwriteup = "	
 <p>
-In the Thursday night game the Broncos smoked the Saints in New Orleans!.
+In the Thursday night game the Rams beat the Vikings!.
 </p> 
 
 <p>
-In Sunday games - The Jaguars crushed the clown world Patriots in Jolly Ole England. The Seahawks beat the Falcons in Atlanta. The Bills beat the Titans. The Bengals upset the Browns in Cleveland. In that game Cleveland's quarterback Deshaun Watson was injured and is lost for the season. The Colts beat the Dolphins. The Packers beat the Texans in a squeaker. The Lions beat the undefeated Vikings in Minnesota. The Eagles mauled the Giants in NY. The Rams beat the Raiders in another close contest. The Chiefs beat the 49ers in San Fransisco. In the night game, the Steelers beat the Jets.
+In Sunday games - The Browns beat the Ravens in an upset. The Lions beat the Titans. The Texans beat the Colts. The Packers beat the Jaguars. The Cardinals beat the Dolphins with a last second field goal. The Patriots upset the Jets. The Falcons beat the Buccaneers. The Commanders beat the Bears in a wild affair featuring a last second Hail Mary pass that won the game! The Chargers zapped the Saints. The Bills trampled the Seahawks. The Eagles beat the Bengals. The Broncos beat the Panthers. The Chiefs beat the Raiders. Is it only me? It seems like lately the Chiefs 12th man on the field is the officiating crew? The 49ers beat the Cowboys.
 </p> 
 
 <p>
-In the Monday night games, The Ravens beat the Buccaneers in Tampa. and the Cardinals beat the Chargers with a last second field goal!
-</p>       
+In the Monday night game the Steelers beat the Giants.
+</p>      
 
 <p>
-A great Seventh week! Don't be disappointed if you did not do well. Bee the ball!
+A great Eighth week! Don't be disappointed if you did not do well. Bee the ball!
 </p>
-
-<p>
-	We have introduced a new member <span style='color:#FF6E3B;font-weight:900;'>ChartMan</span>! ChartMan only picks the winners by looking at the <span style='color:#FF6E3B;font-weight:900;'>Teams Stats</span> popup on the <span style='color:#FF6E3B;font-weight:900;'>Pick your Teams</span> page! It will be interesting to see how he does against those of us (me included) who think through our own picks. Kind of like putting money in stock market indexes verses active investing! 
-</p>
-
 
 <p>
 The Member stats are below! Read em and weep (I know I do)!
