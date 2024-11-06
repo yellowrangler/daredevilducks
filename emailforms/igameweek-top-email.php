e433a37b0fc3f2d8fe3a1b1ae0b73ec9e16ef782
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-11-06 12:29:04 -0500

    season-2024 week-10 start stats more

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,16 +2,16 @@
 
 $emailtopwriteup = "	
 <p>
-In the Thursday night game the Rams beat the Vikings!.
+In the Thursday night game the Texans beat the Jets!.
 </p> 
 
 <p>
-In Sunday games - The Browns beat the Ravens in an upset. The Lions beat the Titans. The Texans beat the Colts. The Packers beat the Jaguars. The Cardinals beat the Dolphins with a last second field goal. The Patriots upset the Jets. The Falcons beat the Buccaneers. The Commanders beat the Bears in a wild affair featuring a last second Hail Mary pass that won the game! The Chargers zapped the Saints. The Bills trampled the Seahawks. The Eagles beat the Bengals. The Broncos beat the Panthers. The Chiefs beat the Raiders. Is it only me? It seems like lately the Chiefs 12th man on the field is the officiating crew? The 49ers beat the Cowboys.
+In Sunday games - The Falcons beat the Cowboys. In a close contest, the Bills beat the Dolphins. The Panthers beat the fading Saints. The Bengals beat the Raiders. The Chargers beat the Browns in Cleveland. The Titans beat the Patriots in spite of some late game heroics by the Pats. The Commanders beat the Giants in New York. The Ravens crushed the Broncos. The Cardinals did an equally appalling beating on the Bears. The Lions looked good as they dispatched the Packers in Green Bay. The Rams beat the Seahawks in overtime in Seattle. The Eagles beat the Jaguars in a close contest. In the night game the Vikings dispatched the Colts. 
 </p> 
 
 <p>
-In the Monday night game the Steelers beat the Giants.
-</p>      
+In the Monday night game the Chiefs beat the Buccaneers in overtime.
+</p>   
 
 <p>
 A great Eighth week! Don't be disappointed if you did not do well. Be the ball!
