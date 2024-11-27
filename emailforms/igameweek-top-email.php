9e24959c3df9a78334fbbeb94068a188bb0eedbd
Tarrant Cutler <tarrant.cutler@gmail.com>
2024-11-26 19:37:11 -0500

    season-2024 week-13 start nostats injury

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,19 +2,19 @@
 
 $emailtopwriteup = "	
 <p>
-In the Thursday night game the Eagles beat their division rivals Commanders!.
+In the Thursday night game the Browns beat the Steelers!.
 </p> 
 
 <p>
-In Sunday games - The Packers beat the Bears in a close contest. The Lions pummeled the Jaguars. The Dolphins beat the fading Raiders. The Saints crushed the Browns. The Rams beat the Patriots in Foxborough. The Vikings beat the Titans. The Steelers beat the Ravens in a close game. The Broncos crushed the Falcons. The Seahawks upset the 49ers in San Fransisco! The Bills beat the previously undefeated Chiefs. The Colts beat the Jets by 1 point. In the late game the Chargers beat the Colts.
+In Sunday games - The Chiefs eked out a win against the lowly Panthers. The Vikings, likewise, held on to beat the Bears in Chicago. The Lions beat the Colts. The Titans, in a bit of an upset, beat the Texans in Houston. The Dolphins beat the Patriots. The Buccaneers beat the Giants in Ny. The Cowboys beat the Commanders in a close contest. The Broncos beat the Raiders. The Packers beat the Purdy-less 49ers. The Seahawks beat the Cardinals. In the late game, the Eagles crushed the Rams. 
 </p> 
 
 <p>
-In the Monday night game the Texans beat the Cowboys!
-</p>   
+In the Monday night game the Ravens beat the Chargers!
+</p>     
 
 <p>
-A great Eleventh week! Don't be disappointed if you did not do well. As Hercule Poirot would say, use your little grey cells!
+A great Twelfth week! Don't be disappointed if you did not do well. As Hercule Poirot would say, use your little grey cells!
 </p>
 
 <p>
