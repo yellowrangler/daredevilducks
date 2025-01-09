ac865c738bff59bcbf9ea77baccf06522f7e2035
Tarrant Cutler <tarrant.cutler@gmail.com>
2025-01-09 17:46:54 -0500

    s2024w19 add shadow img class

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -6,7 +6,7 @@ In the Saturday games the Ravens crushed the Browns. In the night game the Benga
 </p> 
 
 <p>
-In Sunday games - The Panthers beat the Falcons in Atlanta, knocking them out of the playoffs. The Commander beat the Cowboys. The Bears beat the Packers in Green Bay. The Colts beat the Jaguars and the Patriots beat a depleted Bills team. The Eagles beat the Giants. The Buccaneers beat the Saints ensuring a playoff spot. The Texans beat the Titans. The Cardinals beat the 49ers. The Broncos beat the Chiefs in Denver guaranteeing them a playoff spot. The Seahawks beat the Rams. The Chargers beat the Raiders. The Jets beat the Dolphins. In the late night game the Lions beat the Vikings in a thriller.
+In Sunday games - The Panthers beat the Falcons in Atlanta, knocking them out of the playoffs. The Commanders beat the Cowboys. The Bears beat the Packers in Green Bay. The Colts beat the Jaguars and the Patriots beat a depleted Bills team. The Eagles beat the Giants. The Buccaneers beat the Saints ensuring a playoff spot. The Texans beat the Titans. The Cardinals beat the 49ers. The Broncos beat the Chiefs in Denver guaranteeing them a playoff spot. The Seahawks beat the Rams. The Chargers beat the Raiders. The Jets beat the Dolphins. In the late night game the Lions beat the Vikings in a thriller.
 </p> 
 
 <p>
