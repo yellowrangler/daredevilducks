cf20fdb290535d72d9d69c419d02fde48d1ed255
Tarrant Cutler <tarrant.cutler@gmail.com>
2025-01-23 17:03:22 -0500

    s2024 w21 start stats injury bracket

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -1,20 +1,16 @@
 <?php
 
-$emailtopwriteup = "	
+$emailtopwriteup = "
 <p>
-In the Saturday Wildcard games the <span style='font-weight:bold;color:#1280d6;'>Texans</span> beat the <span style='font-weight:bold;color:#1280d6;'>Chargers</span>. In the night game the <span style='font-weight:bold;color:#1280d6;'>Ravens</span> crushed the <span style='font-weight:bold;color:#1280d6;'>Steelers</span>! 
+In the Saturday Divisional games - the <span style='font-weight:bold;color:#1280d6;'>Chiefs</span> beat the <span style='font-weight:bold;color:#1280d6;'>Texans</span> in a game with very critical questionable calls against the Texans. It's hard enough to beat the Chiefs without NFL officiating seemingly being a part of the Chiefs bench! In the night game the <span style='font-weight:bold;color:#1280d6;'>Commanders</span> upset the highly favored <span style='font-weight:bold;color:#1280d6;'>Lions</span>! 
 </p> 
 
 <p>
-In Sunday Wildcard games - The <span style='font-weight:bold;color:#1280d6;'>Bills</span> stampeded over the <span style='font-weight:bold;color:#1280d6;'>Broncos</span>. In spite of the large margin of loss, the Broncos look primed for next year. The <span style='font-weight:bold;color:#1280d6;'>Eagles</span> easily dispatched the <span style='font-weight:bold;color:#1280d6;'>Packers</span>. The <span style='font-weight:bold;color:#1280d6;'>Commanders</span> beat the <span style='font-weight:bold;color:#1280d6;'>Buccaneers</span> in an exciting contest.
-</p> 
+In Sundays Divisional games - The <span style='font-weight:bold;color:#1280d6;'>Eagles</span> beat the <span style='font-weight:bold;color:#1280d6;'>Rams</span> in a rain and snow squall. The <span style='font-weight:bold;color:#1280d6;'>Bills</span> beat the <span style='font-weight:bold;color:#1280d6;'>Ravens</span> in another close and exciting contest!
+</p>  
 
 <p>
-In the Monday night Wildcard game - The <span style='font-weight:bold;color:#1280d6;'>Rams</span> trampled the <span style='font-weight:bold;color:#1280d6;'>Vikings</span>. This was a disappointing loss for a Vikings team that showed so much prowess during the regular season.
-</p>   
-
-<p>
-A great Wildcard week! Bring on the Divisionals!  
+A great Divisional week! Bring on the Conference finals!  
 </p>
 
 <p>
