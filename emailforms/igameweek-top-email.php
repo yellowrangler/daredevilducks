1e42f00b421bd9f4e4421cd64b4c193f15f03e05
Tarrant Cutler <tarrant.cutler@gmail.com>
2026-01-07 12:19:52 -0500

    season-2025 week-19 final

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,27 +2,24 @@
 
 $emailtopwriteup = "
 <p>
-We are entering the final week of the regular 2025 season. Six of the seven playoff brackets (7 for the AFC and 7 for the NFC) are now known. In this last regular season week, we have 4 teams in a pre-playoff win and your in scenario! The <span style='color:#1280D6;font-weight:900;'> Buccaneers</span> and the <span style='color:#1280D6;font-weight:900;'> Panthers</span> will settle who will become the Division Leader for the <span style='color:#1280D6;font-weight:900;'>NFC South</span>. While the <span style='color:#1280D6;font-weight:900;'>Steelers</span> and the <span style='color:#1280D6;font-weight:900;'>Ravens </span> will decide the Division Leader for the <span style='color:#1280D6;font-weight:900;'>AFC North</span>. Gotta love it!  Stay tuned!
+The regular season is over and with apologies to Glen Frey, the heat is on! It's time for the cream to rise to the top! I love this time of the year! The playoff dance card is filled! All that's left is have at it! Play Ball! 
 </p>
 
 <p>
-On the Thursday games the <span style='color:#1280D6;font-weight:900;'>Cowboys</span> beat the <span style='color:#1280D6;font-weight:900;'>Commanders</span>! The <span style='color:#1280D6;font-weight:900;'>Vikings</span> ended a disappointing season for the <span style='color:#1280D6;font-weight:900;'>Lions</span>. The <span style='color:#1280D6;font-weight:900;'>Broncos</span> beat a Mahome-less <span style='color:#1280D6;font-weight:900;'>Chiefs</span>.
+In Saturday's games the <span style='color:#1280D6;font-weight:900;'>Buccaneers</span> beat the <span style='color:#1280D6;font-weight:900;'>Panthers</span>. The <span style='color:#1280D6;font-weight:900;'>Seahawks</span> dispatched the  <span style='color:#1280D6;font-weight:900;'>49ers</span>! 
 </p>
 
 <p>
-In Saturday's games the <span style='color:#1280D6;font-weight:900;'>Texans</span> beat the <span style='color:#1280D6;font-weight:900;'>Chargers</span> in Los Angeles. The <span style='color:#1280D6;font-weight:900;'>Ravens</span> stayed relevant with a win over the <span style='color:#1280D6;font-weight:900;'>Packers</span>! 
+In Sunday's games the <span style='color:#1280D6;font-weight:900;'>Falcons</span> beat the <span style='color:#1280D6;font-weight:900;'>Saints</span>. The <span style='color:#1280D6;font-weight:900;'>Browns</span> beat the <span style='color:#1280D6;font-weight:900;'>Bengals</span> in Cincinnati. The <span style='color:#1280D6;font-weight:900;'>Texans</span> beat the <span style='color:#1280D6;font-weight:900;'>Colts</span>! The <span style='color:#1280D6;font-weight:900;'>Jaguars</span> crushed the <span style='color:#1280D6;font-weight:900;'>Titans</span>! The <span style='color:#1280D6;font-weight:900;'>Vikings</span> beat the <span style='color:#1280D6;font-weight:900;'>Packers</span>. 
 </p>
 
 <p>
-In Sunday's games the <span style='color:#1280D6;font-weight:900;'>Seahawks</span> beat the <span style='color:#1280D6;font-weight:900;'>Panthers</span>. The <span style='color:#1280D6;font-weight:900;'>Bengals</span> beat the <span style='color:#1280D6;font-weight:900;'>Cardinals</span>. The <span style='color:#1280D6;font-weight:900;'>Browns</span> beat the <span style='color:#1280D6;font-weight:900;'>Steelers</span>! The <span style='color:#1280D6;font-weight:900;'>Jaguars</span> crushed the <span style='color:#1280D6;font-weight:900;'>Colts</span> in Indianapolis! The <span style='color:#1280D6;font-weight:900;'>Dolphins</span> beat the <span style='color:#1280D6;font-weight:900;'>Buccaneers</span>. The <span style='color:#1280D6;font-weight:900;'>Patriots</span>  mauled the <span style='color:#1280D6;font-weight:900;'>Jets</span> in New York! The <span style='color:#1280D6;font-weight:900;'>Saints</span> beat the <span style='color:#1280D6;font-weight:900;'>Titans</span> in Tennessee. The <span style='color:#1280D6;font-weight:900;'>Giants</span> erupted on the <span style='color:#1280D6;font-weight:900;'>Raiders</span> in Las Vegas! The <span style='color:#1280D6;font-weight:900;'>Eagles</span> eked by the <span style='color:#1280D6;font-weight:900;'>Bills</span> in Buffalo!
+The <span style='color:#1280D6;font-weight:900;'>Giants</span> mauled the <span style='color:#1280D6;font-weight:900;'>Cowboys</span>! The <span style='color:#1280D6;font-weight:900;'>Bills</span> stampeded the <span style='color:#1280D6;font-weight:900;'>Jets</span>. The <span style='color:#1280D6;font-weight:900;'>Lions</span> beat the <span style='color:#1280D6;font-weight:900;'>Bears</span>! The <span style='color:#1280D6;font-weight:900;'>Broncos</span> crushed the <span style='color:#1280D6;font-weight:900;'>Chargers</span>!
+The <span style='color:#1280D6;font-weight:900;'>Patriots</span> humbled <span style='color:#1280D6;font-weight:900;'>Dolphins</span>! 
 </p>
 
 <p>
-In the Sunday night game the <span style='color:#1280D6;font-weight:900;'>49ers</span> squeaked by the <span style='color:#1280D6;font-weight:900;'>Bears</span>! 
-</p>
-
-<p>
-In the Monday night game the <span style='color:#1280D6;font-weight:900;'>Falcons</span> upset the favored <span style='color:#1280D6;font-weight:900;'>Rams</span>!
+The <span style='color:#1280D6;font-weight:900;'>Commanders</span> mauled the <span style='color:#1280D6;font-weight:900;'>Eagles</span> in Philadelphia! The <span style='color:#1280D6;font-weight:900;'>Raiders</span> beat the <span style='color:#1280D6;font-weight:900;'>Chiefs</span>. The <span style='color:#1280D6;font-weight:900;'>Rams</span> dismissed the <span style='color:#1280D6;font-weight:900;'>Cardinals</span>! In one of the more exciting nail bitters the <span style='color:#1280D6;font-weight:900;'>Steelers</span> beat the <span style='color:#1280D6;font-weight:900;'>Ravens</span> to take take the AFC Northern Division! 
 </p>
 
 <p>
