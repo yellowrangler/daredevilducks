ae215570d2842b4d6d5335cc0203e7a5459c5b39
Tarrant Cutler <tarrant.cutler@gmail.com>
2026-01-14 22:22:01 -0500

    season-2025 week-20 start injuries winterpics brackets stats

--- emailforms/igameweek-top-email.php
+++ emailforms/igameweek-top-email.php
@@ -2,24 +2,31 @@
 
 $emailtopwriteup = "
 <p>
-The regular season is over and with apologies to Glen Frey, the heat is on! It's time for the cream to rise to the top! I love this time of the year! The playoff dance card is filled! All that's left is have at it! Play Ball! 
+What a great early start to the playoffs! The first four games were real nail biters! One can only hope that we get the same level of competitiveness for the rest of the playoffs!
 </p>
 
 <p>
-In Saturday's games the <span style='color:#1280D6;font-weight:900;'>Buccaneers</span> beat the <span style='color:#1280D6;font-weight:900;'>Panthers</span>. The <span style='color:#1280D6;font-weight:900;'>Seahawks</span> dispatched the  <span style='color:#1280D6;font-weight:900;'>49ers</span>! 
+The favored <span style=color:#1280D6;font-weight:900;>Rams</span> beat the underdog <span style=color:#1280D6;font-weight:900;>Panthers</span> in a close game. Though in defeat, the <span style=color:#1280D6;font-weight:900;>Panthers</span> showed that they deserved to be in the playoffs! 
 </p>
 
 <p>
-In Sunday's games the <span style='color:#1280D6;font-weight:900;'>Falcons</span> beat the <span style='color:#1280D6;font-weight:900;'>Saints</span>. The <span style='color:#1280D6;font-weight:900;'>Browns</span> beat the <span style='color:#1280D6;font-weight:900;'>Bengals</span> in Cincinnati. The <span style='color:#1280D6;font-weight:900;'>Texans</span> beat the <span style='color:#1280D6;font-weight:900;'>Colts</span>! The <span style='color:#1280D6;font-weight:900;'>Jaguars</span> crushed the <span style='color:#1280D6;font-weight:900;'>Titans</span>! The <span style='color:#1280D6;font-weight:900;'>Vikings</span> beat the <span style='color:#1280D6;font-weight:900;'>Packers</span>. 
+The <span style=color:#1280D6;font-weight:900;>Bears</span> entered the hazy world of deja vu as they played and beat the <span style=color:#1280D6;font-weight:900;>Packers</span> in a game that was eerily reminiscent to their last game with the <span style=color:#1280D6;font-weight:900;>Packers</span>. All that was missing was overtime! 
 </p>
 
 <p>
-The <span style='color:#1280D6;font-weight:900;'>Giants</span> mauled the <span style='color:#1280D6;font-weight:900;'>Cowboys</span>! The <span style='color:#1280D6;font-weight:900;'>Bills</span> stampeded the <span style='color:#1280D6;font-weight:900;'>Jets</span>. The <span style='color:#1280D6;font-weight:900;'>Lions</span> beat the <span style='color:#1280D6;font-weight:900;'>Bears</span>! The <span style='color:#1280D6;font-weight:900;'>Broncos</span> crushed the <span style='color:#1280D6;font-weight:900;'>Chargers</span>!
-The <span style='color:#1280D6;font-weight:900;'>Patriots</span> humbled <span style='color:#1280D6;font-weight:900;'>Dolphins</span>! 
+  The <span style=color:#1280D6;font-weight:900;>Bills</span> beat the <span style=color:#1280D6;font-weight:900;>Jaguars</span> in an epic confrontation. The <span style=color:#1280D6;font-weight:900;>Bills</span> were able to put together a late drive that sealed the win in this back and forth affair! 
 </p>
 
 <p>
-The <span style='color:#1280D6;font-weight:900;'>Commanders</span> mauled the <span style='color:#1280D6;font-weight:900;'>Eagles</span> in Philadelphia! The <span style='color:#1280D6;font-weight:900;'>Raiders</span> beat the <span style='color:#1280D6;font-weight:900;'>Chiefs</span>. The <span style='color:#1280D6;font-weight:900;'>Rams</span> dismissed the <span style='color:#1280D6;font-weight:900;'>Cardinals</span>! In one of the more exciting nail bitters the <span style='color:#1280D6;font-weight:900;'>Steelers</span> beat the <span style='color:#1280D6;font-weight:900;'>Ravens</span> to take take the AFC Northern Division! 
+The <span style=color:#1280D6;font-weight:900;>49ers</span> beat last years Super Bowl winner <span style=color:#1280D6;font-weight:900;>Eagles</span> in Philadelphia! The <span style=color:#1280D6;font-weight:900;>Eagles</span> looked lost and confused in the second half. The <span style=color:#1280D6;font-weight:900;>49ers</span> on the other hand looked committed and strong! 
+</p>
+
+<p>
+The <span style=color:#1280D6;font-weight:900;>Patriots</span> beat the <span style=color:#1280D6;font-weight:900;>Chargers</span>! The <span style=color:#1280D6;font-weight:900;>Chargers</span> had moments of strength, especially on defense, as they kept the <span style=color:#1280D6;font-weight:900;>Patriots</span> vaulted offense in check for 3 quarters. But then the wheels came off in the fourth quarter as the <span style=color:#1280D6;font-weight:900;>Patriot</span> defense shut down the <span style=color:#1280D6;font-weight:900;>Chargers</span> and the <span style=color:#1280D6;font-weight:900;>Patriots</span> offense kicked in! 
+</p>
+
+<p>
+The <span style=color:#1280D6;font-weight:900;>Texans</span> beat the <span style=color:#1280D6;font-weight:900;>Steelers</span> in Pittsburgh! The <span style=color:#1280D6;font-weight:900;>Steelers</span> kept it close for 3 quarters. But as in the Patriots game, the wheels fell off the <span style=color:#1280D6;font-weight:900;>Steelers</span> cart in the 4th as the <span style=color:#1280D6;font-weight:900;>Texans</span> engine finally found its throttle and took the day.
 </p>
 
 <p>
